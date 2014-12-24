0c47a56191439199dfea8efeed9accdbddf9d417
Tudor Bărăscu <tudor.barascu@qtibia.ro>
2014-12-24 09:04:48 +0200

    added ro values for vl_cistern

--- 02_valuelists/vl_cistern.sql
+++ 02_valuelists/vl_cistern.sql
@@ -11,7 +11,8 @@ CREATE TABLE qwat.vl_cistern (id integer not null, CONSTRAINT "vl_cistern_pk" PR
 ALTER TABLE qwat.vl_cistern ADD COLUMN vl_active boolean default true;
 ALTER TABLE qwat.vl_cistern ADD COLUMN value_en varchar(30) default '' ;
 ALTER TABLE qwat.vl_cistern ADD COLUMN value_fr varchar(30) default '' ;
+ALTER TABLE qwat.vl_cistern ADD COLUMN value_ro varchar(30) default '' ;
 
 /* content */
-INSERT INTO qwat.vl_cistern (id,value_en,value_fr) VALUES (2101,'circular', 'cirvulaire');
-INSERT INTO qwat.vl_cistern (id,value_en,value_fr) VALUES (2102,'rectangular', 'rectangulaire');
+INSERT INTO qwat.vl_cistern (id, value_en, value_fr, value_ro) VALUES (2101,'circular','cirvulaire','circulară');
+INSERT INTO qwat.vl_cistern (id, value_en, value_fr, value_ro) VALUES (2102,'rectangular','rectangulaire','dreptunghiulară');
