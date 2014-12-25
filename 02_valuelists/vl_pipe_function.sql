764318954f369e4a36455a2ee365b8902d933d63
Tudor Bărăscu <tudor.barascu@qtibia.ro>
2014-12-26 00:11:31 +0200

    added ro values for vl_pipe_function

--- 02_valuelists/vl_pipe_function.sql
+++ 02_valuelists/vl_pipe_function.sql
@@ -13,22 +13,23 @@ COMMENT ON TABLE qwat.vl_pipe_function IS 'Function for pipe. Here is determined
 ALTER TABLE qwat.vl_pipe_function ADD COLUMN vl_active boolean default true;
 ALTER TABLE qwat.vl_pipe_function ADD COLUMN value_fr varchar(30) default '';
 ALTER TABLE qwat.vl_pipe_function ADD COLUMN value_en varchar(30) default '';
+ALTER TABLE qwat.vl_pipe_function ADD COLUMN value_ro varchar(30) default '';
 ALTER TABLE qwat.vl_pipe_function ADD COLUMN schema_visible boolean not null default true;
 ALTER TABLE qwat.vl_pipe_function ADD COLUMN code_sire smallint;
 
 /* VALUES */
-INSERT INTO qwat.vl_pipe_function (id,value_fr,schema_visible,code_sire) VALUES (4101, 'Conduite de transport'     ,true , 2);
-INSERT INTO qwat.vl_pipe_function (id,value_fr,schema_visible,code_sire) VALUES (4102, 'Conduite d''hydrant'       ,true , 0);
-INSERT INTO qwat.vl_pipe_function (id,value_fr,schema_visible,code_sire) VALUES (4103, 'Conduite de vidange'       ,false, 4);
-INSERT INTO qwat.vl_pipe_function (id,value_fr,schema_visible,code_sire) VALUES (4104, 'Inconnu'                   ,false, 4);
-INSERT INTO qwat.vl_pipe_function (id,value_fr,schema_visible,code_sire) VALUES (4105, 'Conduite de distribution'  ,true , 2);
-INSERT INTO qwat.vl_pipe_function (id,value_fr,schema_visible,code_sire) VALUES (4106, 'Branchement commun'        ,false, 3);
-INSERT INTO qwat.vl_pipe_function (id,value_fr,schema_visible,code_sire) VALUES (4107, 'By-pass'                   ,false, 2);
-INSERT INTO qwat.vl_pipe_function (id,value_fr,schema_visible,code_sire) VALUES (4108, 'Branchement privé'         ,false, 3);
-INSERT INTO qwat.vl_pipe_function (id,value_fr,schema_visible,code_sire) VALUES (4109, 'Conduite de haute pression',true , 2);
-INSERT INTO qwat.vl_pipe_function (id,value_fr,schema_visible,code_sire) VALUES (4110, 'Drain captant'             ,false, 1);
-INSERT INTO qwat.vl_pipe_function (id,value_fr,schema_visible,code_sire) VALUES (4111, 'Trop plein'                ,true , 4);
-INSERT INTO qwat.vl_pipe_function (id,value_fr,schema_visible,code_sire) VALUES (4112, 'Ventilation'               ,false, 4);
+INSERT INTO qwat.vl_pipe_function (id,value_fr,value_ro,schema_visible,code_sire) VALUES (4101, 'Conduite de transport',	'Conductă de transport',	true, 2);
+INSERT INTO qwat.vl_pipe_function (id,value_fr,value_ro,schema_visible,code_sire) VALUES (4102, 'Conduite d''hydrant',		'Conductă de hidrant',		true, 0);
+INSERT INTO qwat.vl_pipe_function (id,value_fr,value_ro,schema_visible,code_sire) VALUES (4103, 'Conduite de vidange',		'Conductă de golire',		false, 4);
+INSERT INTO qwat.vl_pipe_function (id,value_fr,value_ro,schema_visible,code_sire) VALUES (4104, 'Inconnu',			'Necunoscută',			false, 4);
+INSERT INTO qwat.vl_pipe_function (id,value_fr,value_ro,schema_visible,code_sire) VALUES (4105, 'Conduite de distribution',	'Conductă de distribuţie',	true, 2);
+INSERT INTO qwat.vl_pipe_function (id,value_fr,value_ro,schema_visible,code_sire) VALUES (4106, 'Branchement commun',		'Branşament comun',		false, 3);
+INSERT INTO qwat.vl_pipe_function (id,value_fr,value_ro,schema_visible,code_sire) VALUES (4107, 'By-pass',			'By-pass',			false, 2);
+INSERT INTO qwat.vl_pipe_function (id,value_fr,value_ro,schema_visible,code_sire) VALUES (4108, 'Branchement privé',		'Branşament privat',	       	false, 3);
+INSERT INTO qwat.vl_pipe_function (id,value_fr,value_ro,schema_visible,code_sire) VALUES (4109, 'Conduite de haute pression',	'Conductă de înaltă presiune',	true , 2);
+INSERT INTO qwat.vl_pipe_function (id,value_fr,value_ro,schema_visible,code_sire) VALUES (4110, 'Drain captant',		'Captare dren',             	false, 1);
+INSERT INTO qwat.vl_pipe_function (id,value_fr,value_ro,schema_visible,code_sire) VALUES (4111, 'Trop plein',			'Prea plin',			true , 4);
+INSERT INTO qwat.vl_pipe_function (id,value_fr,value_ro,schema_visible,code_sire) VALUES (4112, 'Ventilation',			'Aerisire', 			false, 4);
 
 
 
