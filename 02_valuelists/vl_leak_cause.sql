54f702ebb21a555ec0d0420bbd71ec722bffe165
Tudor Bărăscu <tudor.barascu@qtibia.ro>
2014-12-25 12:14:56 +0200

    added leak cause ro values

--- 02_valuelists/vl_leak_cause.sql
+++ 02_valuelists/vl_leak_cause.sql
@@ -13,13 +13,14 @@ COMMENT ON TABLE qwat.vl_leak_cause IS 'Causes of leak';
 ALTER TABLE qwat.vl_leak_cause ADD COLUMN vl_active boolean default true;
 ALTER TABLE qwat.vl_leak_cause ADD COLUMN value_fr VARCHAR(30) default '';
 ALTER TABLE qwat.vl_leak_cause ADD COLUMN value_en VARCHAR(30) default '';
+ALTER TABLE qwat.vl_leak_cause ADD COLUMN value_ro VARCHAR(30) default '';
 
 /* VALUES */
-INSERT INTO qwat.vl_leak_cause (id,value_fr ) VALUES (9101,'corrosion');    /* 1  C  */
-INSERT INTO qwat.vl_leak_cause (id,value_fr ) VALUES (9102,'manchon');     /* 2  M  */
-INSERT INTO qwat.vl_leak_cause (id,value_fr ) VALUES (9103,'négligence');  /* 3  F  */
-INSERT INTO qwat.vl_leak_cause (id,value_fr ) VALUES (9104,'pose')   ;     /* 4  P  */
-INSERT INTO qwat.vl_leak_cause (id,value_fr ) VALUES (9105,'sous mur');    /* 5  S  */
+INSERT INTO qwat.vl_leak_cause (id,value_fr,value_ro) VALUES (9101,'corrosion','coroziune');    /* 1  C  */
+INSERT INTO qwat.vl_leak_cause (id,value_fr,value_ro) VALUES (9102,'manchon','manşonare');     	/* 2  M  */
+INSERT INTO qwat.vl_leak_cause (id,value_fr,value_ro) VALUES (9103,'négligence','neglijenţă');  /* 3  F  */
+INSERT INTO qwat.vl_leak_cause (id,value_fr,value_ro) VALUES (9104,'pose','poziţionare')   ;    /* 4  P  */
+INSERT INTO qwat.vl_leak_cause (id,value_fr,value_ro) VALUES (9105,'sous mur','încărcare');     /* 5  S  */
 
 
 
