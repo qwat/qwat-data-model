22a37fecdaa02ba5bc9d1d202c04db3f39a10928
Tudor Bărăscu <tudor.barascu@qtibia.ro>
2014-12-29 11:25:43 +0200

    added ro values for pump type

--- 02_valuelists/vl_pump_type.sql
+++ 02_valuelists/vl_pump_type.sql
@@ -12,9 +12,10 @@ CREATE TABLE qwat.vl_pump_type (id integer not null, CONSTRAINT vl_pump_type_pk 
 ALTER TABLE qwat.vl_pump_type ADD COLUMN vl_active boolean default true;
 ALTER TABLE qwat.vl_pump_type ADD COLUMN value_en  varchar(30) default '' ;
 ALTER TABLE qwat.vl_pump_type ADD COLUMN value_fr  varchar(30) default '' ;
+ALTER TABLE qwat.vl_pump_type ADD COLUMN value_ro  varchar(30) default '' ;
 ALTER TABLE qwat.vl_pump_type ADD COLUMN code_sire smallint               ;
 
 /* CONTENT */
-INSERT INTO qwat.vl_pump_type (id,code_sire,value_en,value_fr) VALUES (2501,0,'','surpresseur');
-INSERT INTO qwat.vl_pump_type (id,code_sire,value_en,value_fr) VALUES (2502,1,'','normal');
-INSERT INTO qwat.vl_pump_type (id,code_sire,value_en,value_fr) VALUES (2503,2,'','accélération');
+INSERT INTO qwat.vl_pump_type (id,code_sire,value_en,value_fr,value_ro) VALUES (2501,0,'','surpresseur','hidrofor');
+INSERT INTO qwat.vl_pump_type (id,code_sire,value_en,value_fr,value_ro) VALUES (2502,1,'','normal','normal');
+INSERT INTO qwat.vl_pump_type (id,code_sire,value_en,value_fr,value_ro) VALUES (2503,2,'','accélération','periferică');
