30d242ed80124d43b14fb1ae17b2229785b5de62
Tudor Bărăscu <tudor.barascu@qtibia.ro>
2014-12-29 11:35:25 +0200

    added ro values for vl_remote

--- 02_valuelists/vl_remote.sql
+++ 02_valuelists/vl_remote.sql
@@ -12,12 +12,13 @@ CREATE TABLE qwat.vl_remote (id integer not null, CONSTRAINT "vl_remote_pk" PRIM
 ALTER TABLE qwat.vl_remote ADD COLUMN vl_active boolean default true;
 ALTER TABLE qwat.vl_remote ADD COLUMN value_fr varchar(30) default '' ;
 ALTER TABLE qwat.vl_remote ADD COLUMN value_en varchar(30) default '' ;
+ALTER TABLE qwat.vl_remote ADD COLUMN value_ro varchar(30) default '' ;
 
 /* content */
-INSERT INTO qwat.vl_remote (id,value_en,value_fr) VALUES (1201,'','Aucune');
-INSERT INTO qwat.vl_remote (id,value_en,value_fr) VALUES (1202,'','Cable en site propre');
-INSERT INTO qwat.vl_remote (id,value_en,value_fr) VALUES (1203,'','Ligne louée');
-INSERT INTO qwat.vl_remote (id,value_en,value_fr) VALUES (1204,'','Ligne commutée');
-INSERT INTO qwat.vl_remote (id,value_en,value_fr) VALUES (1205,'','ADSL');
-INSERT INTO qwat.vl_remote (id,value_en,value_fr) VALUES (1206,'','Radio');
-INSERT INTO qwat.vl_remote (id,value_en,value_fr) VALUES (1207,'','GPRS');
+INSERT INTO qwat.vl_remote (id,value_en,value_fr,value_ro) VALUES (1201,'','Aucune','Niciuna');
+INSERT INTO qwat.vl_remote (id,value_en,value_fr,value_ro) VALUES (1202,'','Cable en site propre','Cablu propriu');
+INSERT INTO qwat.vl_remote (id,value_en,value_fr,value_ro) VALUES (1203,'','Ligne louée','Linie închiriată');
+INSERT INTO qwat.vl_remote (id,value_en,value_fr,value_ro) VALUES (1204,'','Ligne commutée','Linie comutată');
+INSERT INTO qwat.vl_remote (id,value_en,value_fr,value_ro) VALUES (1205,'','ADSL','ADSL');
+INSERT INTO qwat.vl_remote (id,value_en,value_fr,value_ro) VALUES (1206,'','Radio','Radio');
+INSERT INTO qwat.vl_remote (id,value_en,value_fr,value_ro) VALUES (1207,'','GPRS','GPRS');
