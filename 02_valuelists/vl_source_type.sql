1ef3f7b7dbcbf092f0b8ec3f9f93f5577bc76aba
Tudor Bărăscu <tudor.barascu@qtibia.ro>
2014-12-29 12:29:02 +0200

    added source type ro values

--- 02_valuelists/vl_source_type.sql
+++ 02_valuelists/vl_source_type.sql
@@ -12,10 +12,11 @@ CREATE TABLE qwat.vl_source_type (id integer not null, CONSTRAINT "vl_source_typ
 ALTER TABLE qwat.vl_source_type ADD COLUMN vl_active boolean default true;
 ALTER TABLE qwat.vl_source_type ADD COLUMN value_en  varchar(30) default '' ;
 ALTER TABLE qwat.vl_source_type ADD COLUMN value_fr  varchar(30) default '' ;
+ALTER TABLE qwat.vl_source_type ADD COLUMN value_ro  varchar(30) default '' ;
 ALTER TABLE qwat.vl_source_type ADD COLUMN code_sire smallint               ;
 
 /* CONTENT */
-INSERT INTO qwat.vl_source_type (id,code_sire,value_fr) VALUES (2701,0, 'captage eau lac');
-INSERT INTO qwat.vl_source_type (id,code_sire,value_fr) VALUES (2702,1, 'captage eau nappe');
-INSERT INTO qwat.vl_source_type (id,code_sire,value_fr) VALUES (2703,2, 'captage eau source');
-INSERT INTO qwat.vl_source_type (id,code_sire,value_fr) VALUES (2704,3, 'captage eau rivière');
+INSERT INTO qwat.vl_source_type (id,code_sire,value_fr,value_ro) VALUES (2701,0, 'captage eau lac', 'captare lac');
+INSERT INTO qwat.vl_source_type (id,code_sire,value_fr,value_ro) VALUES (2702,1, 'captage eau nappe', 'captare strat freatic');
+INSERT INTO qwat.vl_source_type (id,code_sire,value_fr,value_ro) VALUES (2703,2, 'captage eau source','captare sursă subterană');
+INSERT INTO qwat.vl_source_type (id,code_sire,value_fr,value_ro) VALUES (2704,3, 'captage eau rivière','captare râu');
