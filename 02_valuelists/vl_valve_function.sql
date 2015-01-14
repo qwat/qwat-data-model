ddfd3712f65a4a406d20f42d1cc0b2ca741c83d2
Tudor Bărăscu <tudor.barascu@qtibia.ro>
2015-01-14 12:05:20 +0200

    organe abbone --> vanne de branchement + ro translation

--- 02_valuelists/vl_valve_function.sql
+++ 02_valuelists/vl_valve_function.sql
@@ -21,16 +21,16 @@ ALTER TABLE qwat.vl_valve_function ADD COLUMN short_ro VARCHAR(5) default '';
 ALTER TABLE qwat.vl_valve_function ADD COLUMN schema_visible BOOLEAN NOT NULL default true;
 
 /* VALUES */
-INSERT INTO qwat.vl_valve_function (id, value_fr, value_ro )                 	VALUES (6101,'vanne de régulation','vană de reglaj'); 	/* 1  REG' */
+INSERT INTO qwat.vl_valve_function (id, value_fr, value_ro )                 	VALUES (6101,'vanne de régulation','vană de reglaj'); 		/* 1  REG' */
 INSERT INTO qwat.vl_valve_function (id, value_fr, short_fr, value_ro )       	VALUES (6102,'ventouse','Ve',''); 				/* 2  VE   */
-INSERT INTO qwat.vl_valve_function (id, value_fr, value_ro )       		VALUES (6103,'vanne by-pass','vană by-pass'); 		/* 3  VBP  */
-INSERT INTO qwat.vl_valve_function (id, value_fr, value_ro, schema_visible ) 	VALUES (6104,'vanne d''ouvrage','vană deschidere','false');/* 4  OUV  */
-INSERT INTO qwat.vl_valve_function (id, value_fr, value_ro, schema_visible ) 	VALUES (6105,'organe abonné','','false'); 			/* 5  OA   */
-INSERT INTO qwat.vl_valve_function (id, value_fr, value_ro )                 	VALUES (6106,'prise de secours',''); 			/* 6  SEC  */
+INSERT INTO qwat.vl_valve_function (id, value_fr, value_ro )       		VALUES (6103,'vanne by-pass','vană by-pass'); 			/* 3  VBP  */
+INSERT INTO qwat.vl_valve_function (id, value_fr, value_ro, schema_visible ) 	VALUES (6104,'vanne d''ouvrage','vană deschidere','false');	/* 4  OUV  */
+INSERT INTO qwat.vl_valve_function (id, value_fr, value_ro, schema_visible ) 	VALUES (6105,'vanne de branchement','vană branşament','false'); /* 5  VBR  */
+INSERT INTO qwat.vl_valve_function (id, value_fr, value_ro )                 	VALUES (6106,'prise de secours',''); 				/* 6  SEC  */
 INSERT INTO qwat.vl_valve_function (id, value_fr, value_ro )                 	VALUES (6107,'vanne incendie','vană incendiu'); 		/* 7  VIN  */
 INSERT INTO qwat.vl_valve_function (id, value_fr, value_ro )                 	VALUES (6108,'vanne d''hydrant','vană hidrant'); 		/* 8  VH   */
-INSERT INTO qwat.vl_valve_function (id, value_fr, value_ro )                 	VALUES (6109,'inconnu','necunoscut'); 			/* 9 I     */
-INSERT INTO qwat.vl_valve_function (id, value_fr, short_fr,value_ro )        	VALUES (6110,'vidange','Vi','vană de golire'); 		/* 10 VID  */
+INSERT INTO qwat.vl_valve_function (id, value_fr, value_ro )                 	VALUES (6109,'inconnu','necunoscut'); 				/* 9 I     */
+INSERT INTO qwat.vl_valve_function (id, value_fr, short_fr,value_ro )        	VALUES (6110,'vidange','Vi','vană de golire'); 			/* 10 VID  */
 INSERT INTO qwat.vl_valve_function (id, value_fr, value_ro)                  	VALUES (6111,'organe réseau',''); 				/* 11 VR   */
-INSERT INTO qwat.vl_valve_function (id, value_fr, short_fr, value_ro)        	VALUES (6112,'vidange-ventouse','ViVe',''); 		/* 12 VIVE */
+INSERT INTO qwat.vl_valve_function (id, value_fr, short_fr, value_ro)        	VALUES (6112,'vidange-ventouse','ViVe',''); 			/* 12 VIVE */
 INSERT INTO qwat.vl_valve_function (id, value_fr,value_ro)                   	VALUES (6113,'vanne clapet',''); 				/* 13      */
