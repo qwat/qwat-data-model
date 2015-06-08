30c4ec93d2d9d5605c7d375898db7cfaa47f908c
Denis Rouzaud <denis.rouzaud@gmail.com>
2015-06-08 10:50:08 +0200

    tools: insert vertices to pipe to snap valves

--- tools/snap_valve.sql
+++ tools/snap_valve.sql
@@ -5,3 +5,17 @@ update qwat_od.valve va set geometry = st_snap(va.geometry,pi.geometry,0.02)
 from qwat_od.pipe pi 
 where st_dwithin(va.geometry,pi.geometry, 0.02)
 and va.fk_pipe is null ;
+
+
+-- create vertices to snap valves to
+update qwat_od.pipe pi
+ set geometry = ST_LineMerge(
+           ST_Union(
+             ST_Line_Substring(pi.geometry, 0, ST_Line_Locate_Point(pi.geometry, st_ClosestPoint(va.geometry,pi.geometry))),
+             ST_Line_Substring(pi.geometry, ST_Line_Locate_Point(pi.geometry, st_ClosestPoint(va.geometry,pi.geometry)), 1)
+       ))
+from qwat_od.valve va
+where va.fk_pipe is null
+and st_dwithin(pi.geometry,va.geometry, 0.05);
+
+
