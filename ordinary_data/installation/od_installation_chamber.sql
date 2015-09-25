0858946980d868452b64cc0935772d429033cdf5
Tudor Bărăscu <tudor.barascu@qtibia.ro>
2015-09-25 11:28:34 +0300

    comment the chamber meter column

--- ordinary_data/installation/od_installation_chamber.sql
+++ ordinary_data/installation/od_installation_chamber.sql
@@ -10,7 +10,7 @@ CREATE TABLE qwat_od.chamber();
 /* specific to chambers */
 ALTER TABLE qwat_od.chamber ADD COLUMN id integer NOT NULL REFERENCES qwat_od.installation(id) PRIMARY KEY ;
 ALTER TABLE qwat_od.chamber ADD COLUMN networkseparation   boolean         ;
-ALTER TABLE qwat_od.chamber ADD COLUMN meter               boolean         ;
+ALTER TABLE qwat_od.chamber ADD COLUMN meter               boolean         ; COMMENT ON COLUMN qwat_od.chamber.meter IS 'depicts the presence of a flow meter';
 ALTER TABLE qwat_od.chamber ADD COLUMN depth 	           decimal(10,3)   ;
 ALTER TABLE qwat_od.chamber ADD COLUMN no_valves 	       smallint        ;
 
