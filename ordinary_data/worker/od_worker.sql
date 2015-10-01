7c387c503b3fcc82ef4a5243027f89915a34b014
Denis Rouzaud <denis.rouzaud@gmail.com>
2015-10-01 11:57:50 +0200

    fist last names for workers

--- ordinary_data/worker/od_worker.sql
+++ ordinary_data/worker/od_worker.sql
@@ -8,9 +8,9 @@ CREATE TABLE qwat_od.worker (id serial PRIMARY KEY);
 COMMENT ON TABLE qwat_od.worker IS 'Table for worker.';
 
 
-
 /* COLUMNS */
-ALTER TABLE qwat_od.worker ADD COLUMN name    varchar(40) ;
-ALTER TABLE qwat_od.worker ADD COLUMN date_entry    		date;
-ALTER TABLE qwat_od.worker ADD COLUMN date_end	    		date;
-ALTER TABLE qwat_od.worker ADD COLUMN fk_type    	   integer[];
+ALTER TABLE qwat_od.worker ADD COLUMN last_name  text ;
+ALTER TABLE qwat_od.worker ADD COLUMN first_name text ;
+ALTER TABLE qwat_od.worker ADD COLUMN date_entry date;
+ALTER TABLE qwat_od.worker ADD COLUMN date_end   date;
+ALTER TABLE qwat_od.worker ADD COLUMN fk_type    integer[];
