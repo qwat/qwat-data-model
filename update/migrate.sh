f597ad42f545a30809ced5ab9b7db05f5320ae30
Denis Rouzaud <denis.rouzaud@gmail.com>
2015-10-02 15:22:50 +0200

    migration script

--- update/migrate.sh
+++ update/migrate.sh
@@ -11,10 +11,10 @@ HOST=172.24.171.203
 
 TODAY=`date '+%Y%m%d'`
 
-
 # initialize QWAT
-./init_qwat.sh -p qwat_test -d -v
+./init_qwat.sh -p qwat_test -d
 
+export PGOPTIONS='--client-min-messages=warning'
 
 # CREATE MIGRATION SCHEMA ON DESTINATION AND SOURCE DBs
 /usr/bin/psql --host $HOST --port 5432 -c "CREATE SCHEMA IF NOT EXISTS qwat_migration" $SRCDB $USER
@@ -70,9 +70,124 @@ echo "migrate crossing, leaks..."
 /usr/bin/pg_dump --host $HOST --port 5432 --username "$USER" --format custom --file "data.backup" --table "qwat_od.crossing" "$SRCDB"; /usr/bin/pg_restore  --host $HOST --port 5432 --username "$USER" --dbname "$DESTDB" --no-password --disable-triggers --data-only --single-transaction --exit-on-error "data.backup"
 /usr/bin/pg_dump --host $HOST --port 5432 --username "$USER" --format custom --file "data.backup" --table "qwat_od.leak" "$SRCDB"; /usr/bin/pg_restore  --host $HOST --port 5432 --username "$USER" --dbname "$DESTDB" --no-password --disable-triggers --data-only --single-transaction --exit-on-error "data.backup"
 
-# TODO: meter, part (=>element), samplingpoint, subscriber, valve, meter_reference, subscriber_reference
+# TODO:  meter_reference, subscriber_reference
 
-# hydrantes
+# samplingpoints
+echo "migrate samplingpoints..."
+/usr/bin/psql --host $HOST --port 5432 -c "DROP TABLE IF EXISTS qwat_migration.samplingpoint_data;" $SRCDB $USER
+/usr/bin/psql --host $HOST --port 5432 -c "DROP TABLE IF EXISTS qwat_migration.samplingpoint_data;" $DESTDB $USER
+/usr/bin/psql --host $HOST --port 5432 -c "CREATE TABLE qwat_migration.samplingpoint_data AS (SELECT * FROM qwat_od.samplingpoint)" $SRCDB $USER
+/usr/bin/psql --host $HOST --port 5432 -c "ALTER TABLE qwat_migration.samplingpoint_data ADD COLUMN fk_distributor integer default 1" $SRCDB $USER
+/usr/bin/psql --host $HOST --port 5432 -c "ALTER TABLE qwat_migration.samplingpoint_data ADD COLUMN fk_status integer default 1301" $SRCDB $USER
+/usr/bin/pg_dump --host $HOST --port 5432 --username "$USER" --format custom --file "$TODAY""_samplingpoint_data.backup" --table "qwat_migration.samplingpoint_data" "$SRCDB"
+/usr/bin/pg_restore  --host $HOST --port 5432 --username "$USER" --dbname "$DESTDB" --no-password --disable-triggers --single-transaction --exit-on-error "$TODAY""_samplingpoint_data.backup"
+# get field list
+/usr/bin/psql --host $HOST --port 5432 --tuples-only -o field_list -c "SELECT array_to_string(ARRAY(SELECT attname FROM pg_attribute WHERE attrelid = 'qwat_migration.samplingpoint_data'::regclass AND attnum > 0 ORDER BY attnum ASC), ', ')" $DESTDB $USER
+# remove fields
+sed -i 's/, _district,/,/g' field_list
+sed -i 's/, _pressurezone,/,/g' field_list
+sed -i 's/, fk_node,/,/g' field_list
+# alter columns
+cp field_list src_fields
+cp field_list dest_fields
+sed -i 's/, geometry,/,ST_Force3d(geometry),/g' src_fields
+sed -i 's/, geometry_alt1,/,ST_Force3d(geometry_alt1),/g' src_fields
+sed -i 's/, geometry_alt2/,ST_Force3d(geometry_alt2)/g' src_fields
+echo "INSERT INTO qwat_od.vw_element_samplingpoint (" > cmd.sql
+cat dest_fields >> cmd.sql
+echo ") SELECT " >> cmd.sql
+cat src_fields >> cmd.sql
+echo " FROM qwat_migration.samplingpoint_data;" >> cmd.sql
+/usr/bin/psql  -v ON_ERROR_STOP=on --host $HOST --port 5432 -f cmd.sql $DESTDB $USER
+
+
+# meters
+echo "migrate meters..."
+/usr/bin/psql --host $HOST --port 5432 -c "DROP TABLE IF EXISTS qwat_migration.meter_data;" $SRCDB $USER
+/usr/bin/psql --host $HOST --port 5432 -c "DROP TABLE IF EXISTS qwat_migration.meter_data;" $DESTDB $USER
+/usr/bin/psql --host $HOST --port 5432 -c "CREATE TABLE qwat_migration.meter_data AS (SELECT * FROM qwat_od.meter)" $SRCDB $USER
+/usr/bin/psql --host $HOST --port 5432 -c "ALTER TABLE qwat_migration.meter_data ADD COLUMN fk_distributor integer default 1" $SRCDB $USER
+/usr/bin/pg_dump --host $HOST --port 5432 --username "$USER" --format custom --file "$TODAY""_meter_data.backup" --table "qwat_migration.meter_data" "$SRCDB"
+/usr/bin/pg_restore  --host $HOST --port 5432 --username "$USER" --dbname "$DESTDB" --no-password --disable-triggers --single-transaction --exit-on-error "$TODAY""_meter_data.backup"
+# get field list
+/usr/bin/psql --host $HOST --port 5432 --tuples-only -o field_list -c "SELECT array_to_string(ARRAY(SELECT attname FROM pg_attribute WHERE attrelid = 'qwat_migration.meter_data'::regclass AND attnum > 0 ORDER BY attnum ASC), ', ')" $DESTDB $USER
+# remove fields
+sed -i 's/, _district,/,/g' field_list
+sed -i 's/, _pressurezone,/,/g' field_list
+sed -i 's/, fk_node,/,/g' field_list
+sed -i 's/, _identification_full,/,/g' field_list
+# alter columns
+cp field_list src_fields
+cp field_list dest_fields
+sed -i 's/, geometry,/,ST_Force3d(geometry),/g' src_fields
+sed -i 's/, geometry_alt1,/,ST_Force3d(geometry_alt1),/g' src_fields
+sed -i 's/, geometry_alt2/,ST_Force3d(geometry_alt2)/g' src_fields
+echo "INSERT INTO qwat_od.vw_element_meter (" > cmd.sql
+cat dest_fields >> cmd.sql
+echo ") SELECT " >> cmd.sql
+cat src_fields >> cmd.sql
+echo " FROM qwat_migration.meter_data;" >> cmd.sql
+/usr/bin/psql  -v ON_ERROR_STOP=on --host $HOST --port 5432 -f cmd.sql $DESTDB $USER
+
+# subscribers
+echo "migrate subscribers..."
+/usr/bin/psql --host $HOST --port 5432 -c "DROP TABLE IF EXISTS qwat_migration.subscriber_data;" $SRCDB $USER
+/usr/bin/psql --host $HOST --port 5432 -c "DROP TABLE IF EXISTS qwat_migration.subscriber_data;" $DESTDB $USER
+/usr/bin/psql --host $HOST --port 5432 -c "CREATE TABLE qwat_migration.subscriber_data AS (SELECT * FROM qwat_od.subscriber)" $SRCDB $USER
+/usr/bin/psql --host $HOST --port 5432 -c "ALTER TABLE qwat_migration.subscriber_data ADD COLUMN fk_distributor integer default 1" $SRCDB $USER
+/usr/bin/pg_dump --host $HOST --port 5432 --username "$USER" --format custom --file "$TODAY""_subscriber_data.backup" --table "qwat_migration.subscriber_data" "$SRCDB"
+/usr/bin/pg_restore  --host $HOST --port 5432 --username "$USER" --dbname "$DESTDB" --no-password --disable-triggers --single-transaction --exit-on-error "$TODAY""_subscriber_data.backup"
+# get field list
+/usr/bin/psql --host $HOST --port 5432 --tuples-only -o field_list -c "SELECT array_to_string(ARRAY(SELECT attname FROM pg_attribute WHERE attrelid = 'qwat_migration.subscriber_data'::regclass AND attnum > 0 ORDER BY attnum ASC), ', ')" $DESTDB $USER
+# remove fields
+sed -i 's/, _district,/,/g' field_list
+sed -i 's/, _pressurezone,/,/g' field_list
+sed -i 's/, fk_node,/,/g' field_list
+sed -i 's/, _identification_full,/,/g' field_list
+# alter columns
+cp field_list src_fields
+cp field_list dest_fields
+sed -i 's/, geometry,/,ST_Force3d(geometry),/g' src_fields
+sed -i 's/, geometry_alt1,/,ST_Force3d(geometry_alt1),/g' src_fields
+sed -i 's/, geometry_alt2/,ST_Force3d(geometry_alt2)/g' src_fields
+sed -i 's/, fk_type/,fk_subscriber_type/g' dest_fields
+echo "INSERT INTO qwat_od.vw_element_subscriber (" > cmd.sql
+cat dest_fields >> cmd.sql
+echo ") SELECT " >> cmd.sql
+cat src_fields >> cmd.sql
+echo " FROM qwat_migration.subscriber_data;" >> cmd.sql
+/usr/bin/psql  -v ON_ERROR_STOP=on --host $HOST --port 5432 -f cmd.sql $DESTDB $USER
+
+# meter_references
+echo "migrate meter_references..."
+/usr/bin/psql --host $HOST --port 5432 -c "DROP TABLE IF EXISTS qwat_migration.meter_reference_data;" $SRCDB $USER
+/usr/bin/psql --host $HOST --port 5432 -c "DROP TABLE IF EXISTS qwat_migration.meter_reference_data;" $DESTDB $USER
+/usr/bin/psql --host $HOST --port 5432 -c "CREATE TABLE qwat_migration.meter_reference_data AS (SELECT * FROM qwat_od.meter_reference)" $SRCDB $USER
+/usr/bin/pg_dump --host $HOST --port 5432 --username "$USER" --format custom --file "$TODAY""_meter_reference_data.backup" --table "qwat_migration.meter_reference_data" "$SRCDB"
+/usr/bin/pg_restore  --host $HOST --port 5432 --username "$USER" --dbname "$DESTDB" --no-password --disable-triggers --single-transaction --exit-on-error "$TODAY""_meter_reference_data.backup"
+/usr/bin/psql --host $HOST --port 5432 -c "
+	INSERT INTO qwat_od.meter_reference ( geometry, fk_meter ) 
+	SELECT ref.geometry, meter.id
+	FROM qwat_migration.meter_reference_data ref
+	LEFT JOIN qwat_migration.meter_data old_data ON ref.fk_meter = old_data.id
+	LEFT JOIN qwat_od.vw_element_meter meter ON ST_Equals(old_data.geometry, meter.geometry) IS TRUE; " $DESTDB $USER
+
+# subscriber_references
+echo "migrate subscriber_references..."
+/usr/bin/psql --host $HOST --port 5432 -c "DROP TABLE IF EXISTS qwat_migration.subscriber_reference_data;" $SRCDB $USER
+/usr/bin/psql --host $HOST --port 5432 -c "DROP TABLE IF EXISTS qwat_migration.subscriber_reference_data;" $DESTDB $USER
+/usr/bin/psql --host $HOST --port 5432 -c "CREATE TABLE qwat_migration.subscriber_reference_data AS (SELECT * FROM qwat_od.subscriber_reference)" $SRCDB $USER
+/usr/bin/pg_dump --host $HOST --port 5432 --username "$USER" --format custom --file "$TODAY""_subscriber_reference_data.backup" --table "qwat_migration.subscriber_reference_data" "$SRCDB"
+/usr/bin/pg_restore  --host $HOST --port 5432 --username "$USER" --dbname "$DESTDB" --no-password --disable-triggers --single-transaction --exit-on-error "$TODAY""_subscriber_reference_data.backup"
+/usr/bin/psql --host $HOST --port 5432 -c "
+	INSERT INTO qwat_od.subscriber_reference ( geometry, fk_subscriber ) 
+	SELECT ref.geometry, subscriber.id
+	FROM qwat_migration.subscriber_reference_data ref
+	LEFT JOIN qwat_migration.subscriber_data old_data ON ref.fk_subscriber = old_data.id
+	LEFT JOIN qwat_od.vw_element_subscriber subscriber ON ST_Equals(old_data.geometry, subscriber.geometry) IS TRUE; " $DESTDB $USER
+	
+	
+# hydrants
 echo "migrate hydrants..."
 /usr/bin/psql --host $HOST --port 5432 -c "DROP TABLE IF EXISTS qwat_migration.hydrant_data;" $SRCDB $USER
 /usr/bin/psql --host $HOST --port 5432 -c "DROP TABLE IF EXISTS qwat_migration.hydrant_data;" $DESTDB $USER
@@ -91,13 +206,11 @@ cp field_list dest_fields
 sed -i 's/, geometry,/,ST_Force3d(geometry),/g' src_fields
 sed -i 's/, geometry_alt1,/,ST_Force3d(geometry_alt1),/g' src_fields
 sed -i 's/, geometry_alt2/,ST_Force3d(geometry_alt2)/g' src_fields
-echo "--ALTER TABLE qwat_od.hydrant DISABLE TRIGGER ALL;
-	INSERT INTO qwat_od.vw_element_hydrant (" > cmd.sql
+echo "INSERT INTO qwat_od.vw_element_hydrant (" > cmd.sql
 cat dest_fields >> cmd.sql
 echo ") SELECT " >> cmd.sql
 cat src_fields >> cmd.sql
-echo " FROM qwat_migration.hydrant_data;
-	ALTER TABLE qwat_od.hydrant ENABLE TRIGGER ALL;" >> cmd.sql
+echo " FROM qwat_migration.hydrant_data;" >> cmd.sql
 /usr/bin/psql  -v ON_ERROR_STOP=on --host $HOST --port 5432 -f cmd.sql $DESTDB $USER
 
 # valves
@@ -123,18 +236,45 @@ sed -i 's/, fk_type/,fk_valve_type/g' dest_fields
 sed -i 's/, fk_function/,fk_valve_function/g' dest_fields
 sed -i 's/, fk_node_precision/,fk_precision/g' dest_fields
 sed -i 's/, geometry_handle/,handle_geometry/g' dest_fields
-echo "--ALTER TABLE qwat_od.valve DISABLE TRIGGER ALL;
-	INSERT INTO qwat_od.vw_element_valve (" > cmd.sql
+echo "INSERT INTO qwat_od.vw_element_valve (" > cmd.sql
 cat dest_fields >> cmd.sql
 echo ") SELECT " >> cmd.sql
 cat src_fields >> cmd.sql
-echo " FROM qwat_migration.valve_data;
-	ALTER TABLE qwat_od.valve ENABLE TRIGGER ALL;" >> cmd.sql
+echo " FROM qwat_migration.valve_data;" >> cmd.sql
+/usr/bin/psql  -v ON_ERROR_STOP=on --host $HOST --port 5432 -f cmd.sql $DESTDB $USER
+
+# parts
+echo "migrate parts..."
+/usr/bin/psql --host $HOST --port 5432 -c "DROP TABLE IF EXISTS qwat_migration.part_data;" $SRCDB $USER
+/usr/bin/psql --host $HOST --port 5432 -c "DROP TABLE IF EXISTS qwat_migration.part_data;" $DESTDB $USER
+/usr/bin/psql --host $HOST --port 5432 -c "CREATE TABLE qwat_migration.part_data AS (SELECT * FROM qwat_od.part)" $SRCDB $USER
+/usr/bin/pg_dump --host $HOST --port 5432 --username "$USER" --format custom --file "$TODAY""_part_data.backup" --table "qwat_migration.part_data" "$SRCDB"
+/usr/bin/pg_restore  --host $HOST --port 5432 --username "$USER" --dbname "$DESTDB" --no-password --disable-triggers --single-transaction --exit-on-error "$TODAY""_part_data.backup"
+# get field list
+/usr/bin/psql --host $HOST --port 5432 --tuples-only -o field_list -c "SELECT array_to_string(ARRAY(SELECT attname FROM pg_attribute WHERE attrelid = 'qwat_migration.part_data'::regclass AND attnum > 0 ORDER BY attnum ASC), ', ')" $DESTDB $USER
+# remove fields
+sed -i 's/, _district,/,/g' field_list
+sed -i 's/, _pressurezone,/,/g' field_list
+sed -i 's/, fk_node,/,/g' field_list
+sed -i 's/, orientation,/,/g' field_list
+sed -i 's/, fk_pipe,/,/g' field_list
+# alter columns
+cp field_list src_fields
+cp field_list dest_fields
+sed -i 's/, geometry,/,ST_Force3d(geometry),/g' src_fields
+sed -i 's/, geometry_alt1,/,ST_Force3d(geometry_alt1),/g' src_fields
+sed -i 's/, geometry_alt2/,ST_Force3d(geometry_alt2)/g' src_fields
+sed -i 's/, fk_type/,fk_part_type/g' dest_fields
+echo "INSERT INTO qwat_od.vw_element_part (" > cmd.sql
+cat dest_fields >> cmd.sql
+echo ") SELECT " >> cmd.sql
+cat src_fields >> cmd.sql
+echo " FROM qwat_migration.part_data;" >> cmd.sql
 /usr/bin/psql  -v ON_ERROR_STOP=on --host $HOST --port 5432 -f cmd.sql $DESTDB $USER
 
 
-
 # INSTALLATIONS
+echo "migrate installation..."
 /usr/bin/psql -v ON_ERROR_STOP=on --host $HOST --port 5432 -c "DROP TABLE IF EXISTS qwat_migration.installation_data;" $SRCDB $USER
 /usr/bin/psql -v ON_ERROR_STOP=on --host $HOST --port 5432 -c "DROP TABLE IF EXISTS qwat_migration.installation_data;" $DESTDB $USER
 /usr/bin/psql -v ON_ERROR_STOP=on --host $HOST --port 5432 -c "CREATE TABLE qwat_migration.installation_data AS (SELECT * FROM qwat_od.vw_qwat_installation)" $SRCDB $USER
@@ -154,13 +294,11 @@ cp field_list dest_fields
 sed -i 's/, geometry,/,ST_Force3d(geometry),/g' src_fields
 sed -i 's/, geometry_alt1,/,ST_Force3d(geometry_alt1),/g' src_fields
 sed -i 's/, geometry_alt2,/,ST_Force3d(geometry_alt2),/g' src_fields
-echo "ALTER TABLE qwat_od.node DISABLE TRIGGER ALL;
-	INSERT INTO qwat_od.vw_element_installation (" > cmd.sql
+echo "INSERT INTO qwat_od.vw_element_installation (" > cmd.sql
 cat dest_fields >> cmd.sql
 echo ") SELECT " >> cmd.sql
 cat src_fields >> cmd.sql
-echo " FROM qwat_migration.installation_data;
-	ALTER TABLE qwat_od.node ENABLE TRIGGER ALL;" >> cmd.sql
+echo " FROM qwat_migration.installation_data;" >> cmd.sql
 /usr/bin/psql -v ON_ERROR_STOP=on --host $HOST --port 5432 -f cmd.sql $DESTDB $USER
 
 
