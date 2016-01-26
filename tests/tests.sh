5f24e1de46359c5e4d8836822e14945c57909642
Hugo Mercier <hugo.mercier@oslandia.com>
2016-01-26 14:36:31 +0100

    Add basic unit tests

---
+++ tests/tests.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd ${DIR}/..
+#./init_qwat.sh -p qwat_test -d
+cd -
+
+export PGSERVICE=qwat_test
+
+TESTS="test_add_node.sql test_node_orientation.sql test_altitude.sql"
+
+for f in ${TESTS}; do
+    echo -n "Running $f ... "
+    psql -tA -f ${DIR}/$f >/tmp/test.txt 2>/dev/null
+    diff -u /tmp/test.txt ${f/.sql/.expected.sql} >/dev/null
+    if [ "$?" = "1" ]; then
+        echo "Error"
+        diff -u /tmp/test.txt ${f/.sql/.expected.sql}
+    fi
+    echo "OK"
+done
+
