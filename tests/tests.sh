a04627192f94a9da129ae4f276f38ed1e1aeae00
Denis Rouzaud <denis.rouzaud@gmail.com>
2016-02-12 10:58:54 +0100

    fix path in test and return exit code

--- tests/tests.sh
+++ tests/tests.sh
@@ -31,15 +31,19 @@ export PGOPTIONS="-c lc_messages=C -c client_min_messages=ERROR"
 
 TESTS="test_add_node.sql test_node_orientation.sql test_altitude.sql"
 
+EXITCODE=0
+
 for f in ${TESTS}; do
     echo -n "Running $f ... "
     fo="/tmp/${f}.txt"
     psql -tA -f ${DIR}/$f >$fo 2>&1
-    diff -u $fo ${f/.sql/.expected.sql} >/dev/null
+    diff -u $fo ${DIR}/${f/.sql/.expected.sql} >/dev/null
     if [ "$?" = "1" ]; then
+		EXITCODE=1
         echo "Error"
-        diff -u $fo ${f/.sql/.expected.sql}
+        diff -u $fo ${DIR}/${f/.sql/.expected.sql}
     fi
     echo "OK"
 done
 
+exit $EXITCODE
