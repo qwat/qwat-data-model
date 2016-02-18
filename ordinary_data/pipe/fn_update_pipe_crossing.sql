9b390eb84a94f120538dd027506291943f4359ea
Denis Rouzaud <denis.rouzaud@gmail.com>
2016-02-18 06:40:11 +0100

    fix 3D in pipe crossings

--- ordinary_data/pipe/fn_update_pipe_crossing.sql
+++ ordinary_data/pipe/fn_update_pipe_crossing.sql
@@ -12,27 +12,27 @@ $BODY$
 		_deleted_crossing_count integer := 0;
 	BEGIN
 /* * * * * * * * * * * * * * * * * * * * * * * * */
-		/* compute all crossing points */ 
+		/* compute all crossing points */
 		FOR _crossing IN
 			SELECT pipe1_id, pipe2_id,
 				pipe1_geometry, pipe2_geometry,
-				(dp).geom AS cross_geometry 
-			FROM 
+				(dp).geom AS cross_geometry
+			FROM
 			(
 				/* dump mulitpoints to points */
 				SELECT pipe1_id, pipe2_id,
 					pipe1_geometry, pipe2_geometry,
-					ST_DumpPoints(cross_geometry) AS dp FROM 
+					ST_DumpPoints(cross_geometry) AS dp FROM
 				(
 					/* compute the crossing points only for active pipes and different ids */
 					SELECT p1.id AS pipe1_id,
 						p2.id AS pipe2_id,
 						p1.geometry AS pipe1_geometry,
 						p2.geometry AS pipe2_geometry,
-						ST_Intersection(p1.geometry,p2.geometry) AS cross_geometry 
-					FROM qwat_od.pipe p1, qwat_od.pipe p2 
-					WHERE p1.id < p2.id 
-					AND p1.fk_status = 1301 
+						ST_Intersection(p1.geometry,p2.geometry) AS cross_geometry
+					FROM qwat_od.pipe p1, qwat_od.pipe p2
+					WHERE p1.id < p2.id
+					AND p1.fk_status = 1301
 					AND p2.fk_status = 1301
 					AND ST_Crosses(p1.geometry,p2.geometry)
 				) AS foo
@@ -52,7 +52,7 @@ $BODY$
 				SELECT 	id AS pipe_id,
 					n AS pt_index,
 					ST_LineLocatePoint( geometry, ST_PointN( geometry, n) ) AS pt_locat,
-					( 90 + degrees( ST_Azimuth( ST_PointN( geometry, n), ST_PointN( geometry, n+1 ) ) ) )::integer % 360 AS azimuth 
+					( 90 + degrees( ST_Azimuth( ST_PointN( geometry, n), ST_PointN( geometry, n+1 ) ) ) )::integer % 360 AS azimuth
 				FROM qwat_od.pipe, generate_series(1, ST_NumPoints(geometry)-1) n
 				WHERE pipe.id = _crossing.pipe1_id
 			),
@@ -60,14 +60,14 @@ $BODY$
 			pt_locat AS
 			(
 				SELECT MAX(pt_locat) AS pt_locat
-				FROM azimuth 
+				FROM azimuth
 				WHERE pt_locat < ST_LineLocatePoint( _crossing.pipe1_geometry, _crossing.cross_geometry )
 			)
 			/* get the corresponding azimuth according to segment id (pt_locat) */
 			SELECT azimuth
 			INTO _pipe1
 			FROM azimuth
-			JOIN pt_locat USING (pt_locat);		
+			JOIN pt_locat USING (pt_locat);
 
 /* * * * * * * * * * * * * * * * * * * * * * * * */
 			/* !!!! PIPE 2 !!!! */
@@ -77,7 +77,7 @@ $BODY$
 				SELECT 	id AS pipe_id,
 					n AS pt_index,
 					ST_LineLocatePoint( geometry, ST_PointN( geometry, n) ) AS pt_locat,
-					( 90 + degrees( ST_Azimuth( ST_PointN( geometry, n), ST_PointN( geometry, n+1 ) ) ) )::integer % 360 AS azimuth 
+					( 90 + degrees( ST_Azimuth( ST_PointN( geometry, n), ST_PointN( geometry, n+1 ) ) ) )::integer % 360 AS azimuth
 				FROM qwat_od.pipe, generate_series(1, ST_NumPoints(geometry)-1) n
 				WHERE pipe.id = _crossing.pipe2_id
 			),
@@ -93,7 +93,7 @@ $BODY$
 			INTO _pipe2
 			FROM azimuth
 			JOIN pt_locat USING (pt_locat);
-			
+
 /* * * * * * * * * * * * * * * * * * * * * * * * */
 			/* ERROR REPORTING */
 			IF _pipe1.azimuth IS NULL THEN
@@ -112,20 +112,20 @@ $BODY$
 			/* UPDATE OR INSERT NEW CROSSING */
 			SELECT id FROM qwat_od.crossing WHERE ST_Equals(_crossing.cross_geometry,geometry) IS TRUE LIMIT 1 INTO _crossing_id;
 			IF _crossing_id IS NULL THEN
-				INSERT INTO qwat_od.crossing 
-						(         _pipe1_id,          _pipe2_id, _pipe1_angle,     _pipe2_angle,                 geometry) 
+				INSERT INTO qwat_od.crossing
+						(         _pipe1_id,          _pipe2_id, _pipe1_angle,     _pipe2_angle,                 geometry )
 					VALUES
-						(_crossing.pipe1_id, _crossing.pipe2_id, _pipe1.azimuth, _pipe2.azimuth, _crossing.cross_geometry)
+						(_crossing.pipe1_id, _crossing.pipe2_id, _pipe1.azimuth, _pipe2.azimuth, ST_Force2d(_crossing.cross_geometry) )
 					RETURNING id INTO _crossing_id;
 				_inserted_crossing_count := _inserted_crossing_count + 1;
 			ELSIF update_existing IS TRUE THEN
-				UPDATE qwat_od.crossing 
-				SET 
+				UPDATE qwat_od.crossing
+				SET
 					_pipe1_id     = _crossing.pipe1_id,
 					_pipe1_angle  = _pipe1.azimuth,
 					_pipe2_id     = _crossing.pipe2_id,
 					_pipe2_angle  = _pipe2.azimuth,
-					geometry      = _crossing.cross_geometry
+					geometry      = ST_Force2d(_crossing.cross_geometry)
 				WHERE crossing.id = _crossing_id;
 				_updated_crossing_count := _updated_crossing_count + 1;
 			END IF;
