3335d57be859cc50e3930efba1f9c114769cdbab
Denis Rouzaud <denis.rouzaud@gmail.com>
2016-05-11 08:38:25 +0200

    use python constructor for export_pipe view

---
+++ ordinary_data/views/export/export_pipe.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+import yaml
+import sys
+from sql_export_view import SqlExportView
+
+if len(sys.argv) > 1:
+  pg_service = sys.argv[1]
+else:
+  pg_service = "qwat_test"
+
+definition = yaml.load("""
+
+name: qwat_od.vw_export_pipe
+
+from: qwat_od.pipe
+
+joins:
+  district:
+    table: qwat_od.district
+    fkey: fk_district
+  pressurezone:
+    table: qwat_od.pressurezone
+    fkey: fk_pressurezone
+  precision:
+    table: qwat_vl.precision
+    fkey: fk_precision
+  distributor:
+    table: qwat_od.distributor
+    fkey: fk_distributor
+  status:
+    table: qwat_vl.status
+    fkey: fk_status
+  folder:
+    table: qwat_od.folder
+    fkey: fk_folder
+  function:
+    table: qwat_vl.pipe_function
+    fkey: fk_function
+  installmethod:
+    table: qwat_vl.pipe_installmethod
+    fkey: fk_installmethod
+  material:
+    table: qwat_vl.pipe_material
+    fkey: fk_material
+  protection:
+    table: qwat_vl.pipe_protection
+    fkey: fk_protection
+  node_a:
+    table: qwat_od.node
+    fkey: fk_node_a
+  node_b:
+    table: qwat_od.node
+    fkey: fk_node_b
+
+
+""")
+
+print SqlExportView(pg_service, definition).sql()
--- ordinary_data/views/insert_views.sh
+++ ordinary_data/views/insert_views.sh
@@ -17,7 +17,6 @@ psql -v ON_ERROR_STOP=1 -c "$(./ordinary_data/views/inheritance/od_element_inher
 
 # export
 psql -v ON_ERROR_STOP=1 -v SRID=$SRID -f ordinary_data/views/export/vw_remote.sql
-psql -v ON_ERROR_STOP=1 -v SRID=$SRID -f ordinary_data/views/export/vw_export_pipe.sql
 psql -v ON_ERROR_STOP=1 -v SRID=$SRID -f ordinary_data/views/export/vw_leak.sql
 psql -v ON_ERROR_STOP=1 -v SRID=$SRID -f ordinary_data/views/export/vw_consumptionzone.sql
 psql -v ON_ERROR_STOP=1 -v SRID=$SRID -f ordinary_data/views/export/vw_printmap.sql
@@ -28,6 +27,7 @@ psql -v ON_ERROR_STOP=1 -c "$(./ordinary_data/views/export/export_valve.py ${PGS
 psql -v ON_ERROR_STOP=1 -c "$(./ordinary_data/views/export/export_hydrant.py ${PGSERVICE})"
 psql -v ON_ERROR_STOP=1 -c "$(./ordinary_data/views/export/export_meter.py ${PGSERVICE})"
 psql -v ON_ERROR_STOP=1 -c "$(./ordinary_data/views/export/export_part.py ${PGSERVICE})"
+psql -v ON_ERROR_STOP=1 -c "$(./ordinary_data/views/export/export_pipe.py ${PGSERVICE})"
 psql -v ON_ERROR_STOP=1 -c "$(./ordinary_data/views/export/export_subscriber.py ${PGSERVICE})"
 
 psql -v ON_ERROR_STOP=1 -v SRID=$SRID -f ordinary_data/views/export/search_view.sql
