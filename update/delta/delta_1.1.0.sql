6dfef1947e7c0d628eed5b486ace7bb4e735c0d3
Sylvain Beorchia <sylvain.beorchia@oslandia.com>
2016-09-26 10:59:20 +0200

    Update delta file for valves

--- update/delta/delta_1.1.0.sql
+++ update/delta/delta_1.1.0.sql
@@ -40,7 +40,7 @@ CREATE INDEX valve_geoidx ON qwat_od.valve USING GIST ( geometry );
 CREATE INDEX valve_geoidx_alt1 ON qwat_od.valve USING GIST ( geometry_alt1 );
 CREATE INDEX valve_geoidx_alt2 ON qwat_od.valve USING GIST ( geometry_alt2 );
 
-ALTER TABLE qwat_od.valve ALTER COLUMN id serial;
+-- ALTER TABLE qwat_od.valve ALTER COLUMN id serial;
 -- integer NOT NULL REFERENCES qwat_od.network_element(id) PRIMARY KEY;
 
 ALTER TABLE qwat_od.valve ADD COLUMN year          smallint CHECK (year     IS NULL OR year     > 1800 AND year     < 2100);
@@ -65,4 +65,70 @@ CREATE TRIGGER valve_set_orientation
     FOR EACH ROW
     EXECUTE PROCEDURE qwat_od.ft_valve_set_orientation();
 COMMENT ON TRIGGER valve_set_orientation ON qwat_od.valve IS 'Trigger: set orientation after inserting a valve.';
+
+
+CREATE OR REPLACE FUNCTION qwat_od.ft_valve_add_pipe_vertex()
+  RETURNS trigger AS
+$BODY$
+    DECLARE
+        pipe_id integer;
+    BEGIN
+            -- add a vertex to the corresponding pipe if it intersects
+            -- when the valve is close enough to the pipe (< 1 micrometer) the valve is considered to intersect the pipe
+            -- it allows to deal with intersections that cannot be represented by floating point numbers
+            UPDATE qwat_od.pipe SET geometry = ST_Snap(geometry, NEW.geometry, 1e-6) WHERE ST_Distance(geometry, NEW.geometry) < 1e-6;
+            PERFORM qwat_od.fn_valve_set_orientation(NEW.id);
+        RETURN NEW;
+    END;
+$BODY$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER tr_valve_add_pipe_vertex_insert
+  AFTER INSERT
+  ON qwat_od.valve
+  FOR EACH ROW
+  EXECUTE PROCEDURE qwat_od.ft_valve_add_pipe_vertex();
+COMMENT ON TRIGGER tr_valve_add_pipe_vertex_insert ON qwat_od.valve IS 'Trigger: updates auto fields after insert.';
+
+CREATE TRIGGER tr_valve_add_pipe_vertex_update
+  AFTER UPDATE OF geometry
+  ON qwat_od.valve
+  FOR EACH ROW
+  WHEN (ST_Equals(ST_Force2d(NEW.geometry), ST_Force2d(OLD.geometry)) IS FALSE )
+  EXECUTE PROCEDURE qwat_od.ft_valve_add_pipe_vertex();
+COMMENT ON TRIGGER tr_valve_add_pipe_vertex_update ON qwat_od.valve IS 'Trigger: updates auto fields after geom update.';
+
+
+CREATE OR REPLACE FUNCTION qwat_od.ft_valve_geom() RETURNS TRIGGER AS
+$BODY$
+    BEGIN
+        NEW.fk_pipe             := qwat_od.fn_pipe_get_id(NEW.geometry);
+        NEW.fk_district         := qwat_od.fn_get_district(NEW.geometry);
+        NEW.fk_pressurezone     := qwat_od.fn_get_pressurezone(NEW.geometry);
+        RETURN NEW;
+    END;
+$BODY$
+LANGUAGE plpgsql;
+COMMENT ON FUNCTION qwat_od.ft_valve_geom() IS 'Trigger: when inserting or updating a valve, assign pipe and geom infos.';
+
+CREATE TRIGGER tr_valve_infos_insert_trigger
+    BEFORE INSERT ON qwat_od.valve
+    FOR EACH ROW
+    EXECUTE PROCEDURE qwat_od.ft_valve_geom();
+COMMENT ON TRIGGER tr_valve_infos_insert_trigger ON qwat_od.valve IS 'Trigger: when inserting a valve, assign pipe.';
+
+
+CREATE TRIGGER tr_valve_infos_update_trigger
+    BEFORE UPDATE ON qwat_od.valve
+    FOR EACH ROW
+     WHEN (NOT ST_Equals(OLD.geometry, NEW.geometry))
+    EXECUTE PROCEDURE qwat_od.ft_valve_geom();
+COMMENT ON TRIGGER tr_valve_infos_update_trigger ON qwat_od.valve IS 'Trigger: when updating a valve, assign pipe.';
+
+
+
+
+
+DROP VIEW qwat_od.vw_element_valve;
+DROP VIEW qwat_od.vw_export_valve;
 
