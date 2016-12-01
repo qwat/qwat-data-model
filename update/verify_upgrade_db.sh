f0d707ec120f7a9550d13442a50aaa0bf8f34684
Denis Rouzaud <denis.rouzaud@gmail.com>
2016-12-01 11:41:03 +0100

    use ON_ERROR_STOP when running deltas

--- update/verify_upgrade_db.sh
+++ update/verify_upgrade_db.sh
@@ -80,7 +80,7 @@ do
     CURRENT_DELTA_NUM_VERSION=$(echo $CURRENT_DELTA| cut -c 7)
     if [[ $CURRENT_DELTA_NUM_VERSION > $SHORT_LATEST_TAG || $CURRENT_DELTA_NUM_VERSION = $SHORT_LATEST_TAG || $SHORT_LATEST_TAG = '' ]]; then
         printf "    Processing ${GREEN}$CURRENT_DELTA${NC}, num version = $CURRENT_DELTA_NUM_VERSION\n"
-        /usr/bin/psql --host $HOST --port 5432 --username "$USER" --no-password -q -d "$TESTCONFORMDB" -f $f
+        /usr/bin/psql -v ON_ERROR_STOP=1 --host $HOST --port 5432 --username "$USER" --no-password -q -d "$TESTCONFORMDB" -f $f
     else
         printf "    Bypassing  ${RED}$CURRENT_DELTA${NC}, num version = $CURRENT_DELTA_NUM_VERSION\n"
     fi
