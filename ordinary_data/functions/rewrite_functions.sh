a56ee8787db262b2dd7aad2f702f16e6c6063cb3
Denis Rouzaud <denis.rouzaud@gmail.com>
2016-12-01 09:43:02 +0100

    fix path in shell script

--- ordinary_data/functions/rewrite_functions.sh
+++ ordinary_data/functions/rewrite_functions.sh
@@ -2,7 +2,9 @@
 
 set -e
 
-for f in *.sql
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+for f in ${DIR}/*.sql
 do
   echo "Updating function $f"
   psql -v ON_ERROR_STOP=1 -v SRID=$SRID -f $f
