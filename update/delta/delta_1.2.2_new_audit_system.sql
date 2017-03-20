fc4774296cf3b1ad19413e1894a80ba17d3bf02d
Régis Haubourg <regis.haubourg@oslandia.com>
2017-03-20 20:38:06 +0100

    fixed audit views for init and update process using post delta

--- update/delta/delta_1.2.2_new_audit_system.sql
+++ update/delta/delta_1.2.2_new_audit_system.sql
@@ -347,58 +347,8 @@ DROP TRIGGER IF EXISTS audit_trigger_stm ON qwat_od.subscriber_reference;
 DROP TRIGGER IF EXISTS audit_trigger_stm ON qwat_od.surveypoint;
 
 
-DO $$
-BEGIN
-PERFORM qwat_sys.audit_table('qwat_od.valve');
-
--- PERFORM qwat_sys.audit_view('qwat_od.vw_consumptionzone', 'true'::boolean, '{}'::text[], '{id}'::text[]);
-
-PERFORM qwat_sys.audit_view('qwat_od.vw_element_hydrant', 'true'::boolean, '{}'::text[], '{id}'::text[]);
-PERFORM qwat_sys.audit_view('qwat_od.vw_element_installation', 'true'::boolean, '{}'::text[], '{id}'::text[]);
-PERFORM qwat_sys.audit_view('qwat_od.vw_element_meter', 'true'::boolean, '{}'::text[], '{id}'::text[]);
-PERFORM qwat_sys.audit_view('qwat_od.vw_element_part', 'true'::boolean, '{}'::text[], '{id}'::text[]);
-PERFORM qwat_sys.audit_view('qwat_od.vw_element_samplingpoint', 'true'::boolean, '{}'::text[], '{id}'::text[]);
-PERFORM qwat_sys.audit_view('qwat_od.vw_element_subscriber', 'true'::boolean, '{}'::text[], '{id}'::text[]);
-
--- PERFORM qwat_sys.audit_view('qwat_od.vw_export_hydrant', 'true'::boolean, '{}'::text[], '{id}'::text[]);
--- PERFORM qwat_sys.audit_view('qwat_od.vw_export_installation', 'true'::boolean, '{}'::text[], '{id}'::text[]);
--- PERFORM qwat_sys.audit_view('qwat_od.vw_export_meter', 'true'::boolean, '{}'::text[], '{id}'::text[]);
--- PERFORM qwat_sys.audit_view('qwat_od.vw_export_part', 'true'::boolean, '{}'::text[], '{id}'::text[]);
--- PERFORM qwat_sys.audit_view('qwat_od.vw_export_subscriber', 'true'::boolean, '{}'::text[], '{id}'::text[]);
--- PERFORM qwat_sys.audit_view('qwat_od.vw_export_valve', 'true'::boolean, '{}'::text[], '{id}'::text[]);
-
-PERFORM qwat_sys.audit_view('qwat_od.vw_installation_chamber', 'true'::boolean, '{}'::text[], '{id}'::text[]);
-PERFORM qwat_sys.audit_view('qwat_od.vw_installation_pressurecontrol', 'true'::boolean, '{}'::text[], '{id}'::text[]);
-PERFORM qwat_sys.audit_view('qwat_od.vw_installation_pump', 'true'::boolean, '{}'::text[], '{id}'::text[]);
-PERFORM qwat_sys.audit_view('qwat_od.vw_installation_source', 'true'::boolean, '{}'::text[], '{id}'::text[]);
-PERFORM qwat_sys.audit_view('qwat_od.vw_installation_tank', 'true'::boolean, '{}'::text[], '{id}'::text[]);
-PERFORM qwat_sys.audit_view('qwat_od.vw_installation_treatment', 'true'::boolean, '{}'::text[], '{id}'::text[]);
-
--- PERFORM qwat_sys.audit_view('qwat_od.vw_leak', 'true'::boolean, '{}'::text[], '{id}'::text[]);
--- PERFORM qwat_sys.audit_view('qwat_od.vw_node_element', 'true'::boolean, '{}'::text[], '{id}'::text[]);
--- PERFORM qwat_sys.audit_view('qwat_od.vw_pipe', 'true'::boolean, '{}'::text[], '{id}'::text[]);
--- PERFORM qwat_sys.audit_view('qwat_od.vw_pipe_child_parent', 'true'::boolean, '{}'::text[], '{id}'::text[]);
--- PERFORM qwat_sys.audit_view('qwat_od.vw_pipe_schema_merged', 'true'::boolean, '{}'::text[], '{id}'::text[]);
--- PERFORM qwat_sys.audit_view('qwat_od.vw_pipe_schema', 'true'::boolean, '{}'::text[], '{id}'::text[]);
--- PERFORM qwat_sys.audit_view('qwat_od.vw_pipe_schema_error', 'true'::boolean, '{}'::text[], '{id}'::text[]);
--- PERFORM qwat_sys.audit_view('qwat_od.vw_pipe_schema_visibleitems', 'true'::boolean, '{}'::text[], '{id}'::text[]);
--- PERFORM qwat_sys.audit_view('qwat_od.vw_printmap', 'true'::boolean, '{}'::text[], '{id}'::text[]);
--- PERFORM qwat_sys.audit_view('qwat_od.vw_protectionzone', 'true'::boolean, '{}'::text[], '{id}'::text[]);
-
-PERFORM qwat_sys.audit_view('qwat_od.vw_qwat_installation', 'true'::boolean, '{}'::text[], '{id}'::text[]);
-PERFORM qwat_sys.audit_view('qwat_od.vw_qwat_network_element', 'true'::boolean, '{}'::text[], '{id}'::text[]);
-PERFORM qwat_sys.audit_view('qwat_od.vw_qwat_node', 'true'::boolean, '{}'::text[], '{id}'::text[]);
-
--- PERFORM qwat_sys.audit_view('qwat_od.vw_remote', 'true'::boolean, '{}'::text[], '{id}'::text[]);
--- PERFORM qwat_sys.audit_view('qwat_od.vw_search_view', 'true'::boolean, '{}'::text[], '{id}'::text[]);
--- PERFORM qwat_sys.audit_view('qwat_od.vw_subscriber_pipe_relation', 'true'::boolean, '{}'::text[], '{id}'::text[]);
--- PERFORM qwat_sys.audit_view('qwat_od.vw_valve_lines', 'true'::boolean, '{}'::text[], '{id}'::text[]);
-
-END
-$$;
-
 
 -- finishes with upgrading qwat version
 
-UPDATE qwat_sys.versions SET version = '1.1.1';
+UPDATE qwat_sys.versions SET version = '1.2.2';
 
