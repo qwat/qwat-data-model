1461d2a5a38df8e27853c9b4a8ed2f08eb9e667e
Régis Haubourg <regis.haubourg@oslandia.com>
2017-03-21 17:58:35 +0100

    replace old diagram with schemaspy web doc

---
+++ diagram/schema_py_exec.sh
@@ -0,0 +1,3 @@
+#requires schemaSpy http://schemaspy.sourceforge.net/
+
+java -jar schemaSpy_5.0.0.jar -t pgsql \-all -hq -db qwat -u postgres -p postgres \-host localhost -o /home/regis/OSLANDIA/projets_locaux/QWAT/schemaspy \-dp /usr/share/java/postgresql-jdbc4-9.2.jar
