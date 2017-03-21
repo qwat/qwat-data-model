3778b96b8d6386956b96a2be6fb9db1ca1631a5d
Sylvain Beorchia <sylvain.beorchia@oslandia.com>
2017-03-21 15:54:05 +0100

    Debug mode

--- update/test_migration.py
+++ update/test_migration.py
@@ -57,11 +57,14 @@ def _execute_statements(cur, fileName, diff_exe):
         print 'DataModel is OK'
     else:
         print 'DataModel is NOT conform'
+        # For debug purposes
+        print "=========================================================================="
+        print buffer_expected.replace('%','') # TODO Debug purpose
+        print "--------------------------------------------------------------------------"
+        print buffer.replace('%','') # TODO Debug purpose
+        print ".........................................................................."
         print 'Diff:'
         os.system('{diff} -U 0 --ignore-all-space --ignore-blank-lines test_migration.expected.sql output.sql'.format(diff=diff_exe))
-        #print buffer_expected.replace('%','') # TODO Debug purpose
-        #print buffer.replace('%','') # TODO Debug purpose
-        # os.system('{diff} --ignore-all-space --ignore-blank-lines --side-by-side test_migration.expected.sql output.sql'.format(diff=diff_exe))
 
 if __name__ == "__main__":
     """
