0db584bd939b6d8382d11a0e8dcb4730532f002a
Sylvain Beorchia <sylvain.beorchia@oslandia.com>
2017-04-03 15:22:43 +0200

    trying to fix issue when pushing new data sample DUMP

--- update/verify_upgrade_db.sh
+++ update/verify_upgrade_db.sh
@@ -264,7 +264,7 @@ if [[ $EXITCODE == 0 ]]; then
     git add $FILE_NAME_STRUCT
     git add $FILE_NAME_VL
     git commit -m "Update data-sample"
-    git push
+    git push | head -n -2 -
 #     git push https://$GH_TOKEN@github.com/qwat/qwat-data-sample.git
 #     git push https://$QWAT_USER@github.com/qwat/qwat-data-sample.git
 
