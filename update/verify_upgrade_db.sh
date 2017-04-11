c769845c0735c0985d8abb5bb31a8a16bf51073e
Sylvain Beorchia <sylvain.beorchia@oslandia.com>
2017-04-11 09:47:01 +0200

    Display output for debug

--- update/verify_upgrade_db.sh
+++ update/verify_upgrade_db.sh
@@ -186,7 +186,8 @@ if [[ $EXITCODE == 0 ]]; then
         do
             if [[ "$f" == *_data_and_structure_sample.backup ]]; then
                 printf "\n${YELLOW}   Restoring $f ${NC}\n"
-                /usr/bin/pg_restore --host $HOST --port 5432 --username "$USER"  --no-password --dbname "$DEMODB" --verbose "$f" >output.txt 2>&1
+                #/usr/bin/pg_restore --host $HOST --port 5432 --username "$USER"  --no-password --dbname "$DEMODB" --verbose "$f" >output.txt 2>&1
+                /usr/bin/pg_restore --host $HOST --port 5432 --username "$USER"  --no-password --dbname "$DEMODB" --verbose "$f"  # For debuging
             fi
         done
         echo "Done"
