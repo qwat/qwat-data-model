dbc31f1e1b5a00afa08241165274b75c6d4c482b
Tudor Bărăscu <tudor.barascu@qtibia.ro>
2017-07-07 21:07:08 +0300

    add fk_object_reference to survey points

--- ordinary_data/surveypoint/od_surveypoint.sql
+++ ordinary_data/surveypoint/od_surveypoint.sql
@@ -16,18 +16,18 @@ ALTER TABLE qwat_od.surveypoint ADD COLUMN description      text;
 ALTER TABLE qwat_od.surveypoint ADD COLUMN date             date;
 ALTER TABLE qwat_od.surveypoint ADD COLUMN fk_folder        integer ;
 ALTER TABLE qwat_od.surveypoint ADD COLUMN altitude         decimal(10,3) default null;
+ALTER TABLE qwat_od.surveypoint ADD COLUMN fk_object_reference integer;
+
 
 /* GEOMETRY */
 ALTER TABLE qwat_od.surveypoint ADD COLUMN geometry         geometry(POINTZ,:SRID);
 CREATE INDEX surveypoint_geoidx ON qwat_od.surveypoint USING GIST ( geometry );
 
--- TODO add fk_object_reference
-
 /* constraints */
 ALTER TABLE qwat_od.surveypoint ADD CONSTRAINT surveypoint_fk_type   FOREIGN KEY (fk_survey_type) REFERENCES qwat_vl.survey_type(id) MATCH FULL; CREATE INDEX fki_surveypoint_fk_type   ON qwat_od.surveypoint(fk_survey_type);
 ALTER TABLE qwat_od.surveypoint ADD CONSTRAINT surveypoint_fk_worker FOREIGN KEY (fk_worker)      REFERENCES qwat_od.worker(id)      MATCH FULL; CREATE INDEX fki_surveypoint_fk_worker ON qwat_od.surveypoint(fk_worker);
 ALTER TABLE qwat_od.surveypoint ADD CONSTRAINT surveypoint_fk_folder FOREIGN KEY (fk_folder)      REFERENCES qwat_od.folder(id)      MATCH FULL; CREATE INDEX fki_surveypoint_fk_folder ON qwat_od.surveypoint(fk_folder);
-
+ALTER TABLE qwat_od.surveypoint ADD CONSTRAINT surveypoint_fk_object_reference FOREIGN KEY (fk_object_reference) REFERENCES qwat_vl.object_reference (id) MATCH FULL;
 
 /* Altitude triggers */
 
