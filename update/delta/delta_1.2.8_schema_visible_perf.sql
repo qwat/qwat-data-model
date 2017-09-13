f06ed57ec85ad1bf826d232351a7e242b859e91b
Denis Rouzaud <denis.rouzaud@gmail.com>
2017-09-13 15:23:06 +0200

    add hardcoded _schema_visible field to pipes and valves for enhanced perfomance on QGIS
    This creates a field _schema_visible which determines if
    an element is visible in schematic view.
    It is calculated by COALESCE(schema_force_visible, value_list.schema_visible).
    This allows direct use of this field in QGIS for rule-based labeling and
    fixes performance issues if calculation is done on client side.
    
    * schema_visible field is dropped on network_element as it is not used (was introduced for valves at the origin)
    * _schema_visible and schema_force_visible are added to pipe and valve tables by the qwat_sys.fn_enable_schemavisible function
    * old function qwat_sys.fn_enable_schemaview is enhanced and renamed

--- update/delta/delta_1.2.8_schema_visible_perf.sql
+++ update/delta/delta_1.2.8_schema_visible_perf.sql
@@ -1,9 +1,26 @@
 /*
-	qWat - QGIS Water Module
+This creates a field _schema_visible which determines if
+an element is visible in schematic view.
+It is calculated by COALESCE(schema_force_visible, value_list.schema_visible).
+This allows direct use of this field in QGIS for rule-based labeling and
+fixes performance issues if calculation is done on client side.
 
-	SQL file :: function to add schema view capability to tables
+* schema_visible field is dropped on network_element as it is not used (was introduced for valves at the origin)
+* _schema_visible and schema_force_visible are added to pipe and valve tables by the qwat_sys.fn_enable_schemavisible function
+* old function qwat_sys.fn_enable_schemaview is enhanced and renamed
 */
+BEGIN;
 
+ALTER TABLE qwat_od.network_element DROP COLUMN schema_force_visible;
+
+DROP FUNCTION qwat_sys.fn_enable_schemaview(text);
+
+ALTER TABLE qwat_od.pipe RENAME COLUMN schema_force_visible TO schema_force_visible_old;
+ALTER TABLE qwat_od.pipe DROP CONSTRAINT pipe_schema_force_visible;
+DROP INDEX qwat_od.fki_pipe_schema_force_visible;
+ALTER TABLE qwat_od.valve RENAME COLUMN schema_force_visible TO schema_force_visible_old;
+ALTER TABLE qwat_od.valve DROP CONSTRAINT valve_schema_force_visible;
+DROP INDEX qwat_od.fki_valve_schema_force_visible;
 
 CREATE OR REPLACE FUNCTION qwat_sys.fn_enable_schemavisible(_table_name text, _vl_table text, _fk_field text)
 	RETURNS VOID AS
@@ -58,3 +75,16 @@ END;
 $BODY$
 LANGUAGE plpgsql;
 COMMENT ON FUNCTION qwat_sys.fn_enable_schemavisible(text,text,text) IS 'Add a column schema_force_visible used to overwrite the schema_view defined by the auxiliary table (pipe_function for instance). Add a column _schema_visible which determines if the element is visible in the schema (updated by trigger).';
+
+
+DO $$ BEGIN PERFORM qwat_sys.fn_enable_schemavisible('valve', 'valve_function', 'fk_valve_function'); END $$;
+DO $$ BEGIN PERFORM qwat_sys.fn_enable_schemavisible('pipe', 'pipe_function', 'fk_function'); END $$;
+
+UPDATE qwat_od.pipe SET schema_force_visible = schema_force_visible_old;
+UPDATE qwat_od.valve SET schema_force_visible = schema_force_visible_old;
+
+ALTER TABLE qwat_od.pipe DROP COLUMN schema_force_visible_old;
+ALTER TABLE qwat_od.valve DROP COLUMN schema_force_visible_old;
+
+
+COMMIT;
