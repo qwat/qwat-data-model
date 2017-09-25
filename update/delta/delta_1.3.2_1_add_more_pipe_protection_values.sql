eb3fe1ebbd48057d88ab1dcc4bf5a5cd04fb8664
Tudor Bărăscu <tudor.barascu@qtibia.ro>
2017-09-25 14:49:23 +0300

    add delta for pipe protection

---
+++ update/delta/delta_1.3.2_1_add_more_pipe_protection_values.sql
@@ -0,0 +1,3 @@
+INSERT INTO qwat_vl.pipe_protection (id,value_fr,value_ro) VALUES (4313,'conduite AC','conductă OL');                           /* TB_id: 13 */
+INSERT INTO qwat_vl.pipe_protection (id,value_fr,value_ro) VALUES (4314,'conduite PE','conductă PE');                           /* TB_id: 14 */
+INSERT INTO qwat_vl.pipe_protection (id,value_fr,value_ro) VALUES (4315,'conduite PVC','conductă PVC');                         /* TB_id: 15 */
