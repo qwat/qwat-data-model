e2e020d1da827ea02d51561764326ab5f73118e9
Mario Baranzini <marioba@users.noreply.github.com>
2017-09-25 17:06:20 +0200

    Add pum integration to manage database upgrades
    Add pum (https://github.com/opengisch/pum) as a submodule and configure Travis to check db migrations with pum.

---
+++ update/delta/delta_1.2.8_upgrades_table.sql
@@ -0,0 +1,22 @@
+
+-- adds informations table used by the new db_manager
+
+CREATE TABLE IF NOT EXISTS qwat_sys.upgrades
+                (
+                id serial NOT NULL,
+                version character varying(50),
+                description character varying(200) NOT NULL,
+                type integer NOT NULL,
+                script character varying(1000) NOT NULL,
+                checksum character varying(32) NOT NULL,
+                installed_by character varying(100) NOT NULL,
+                installed_on timestamp without time zone NOT NULL DEFAULT now(),
+                execution_time integer NOT NULL,
+                success boolean NOT NULL,
+                CONSTRAINT upgrades_pk PRIMARY KEY (id)
+                );
+
+
+-- finishes with upgrading qwat version
+
+UPDATE qwat_sys.versions SET version = '1.2.8';
