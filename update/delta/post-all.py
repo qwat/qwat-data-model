9b8f28bfe8edf777ebf9dc5a93e6d12c27be61e2
Sylvain Beorchia <sylvain.beorchia@oslandia.com>
2017-10-25 09:20:25 +0200

    Fixing views drop in deltas

---
+++ update/delta/post-all.py
@@ -0,0 +1,21 @@
+from pum.core.deltapy import DeltaPy
+import os
+
+class RecreateViewsAndFunctions(DeltaPy):
+
+    def run(self):
+
+        delta_dir = self.delta_dir
+        """
+        SRID=$SRID ./ordinary_data/views/rewrite_views.sh
+        SRID=$SRID ./ordinary_data/functions/rewrite_functions.sh
+        cd $DIR
+        """
+        self.write_message("Reloading views and functions")
+
+        views_sh = "{}../../ordinary_data/views/rewrite_views.sh".format(self.delta_dir)
+        functions_sh = "{}../../ordinary_data/functions/rewrite_functions.sh".format(self.delta_dir)
+
+        # Execute commands
+        os.system(views_sh)
+        os.system(functions_sh)
---
+++ update/delta/pre-all.py
@@ -0,0 +1,25 @@
+from pum.core.deltapy import DeltaPy
+import os
+
+class DropViews(DeltaPy):
+
+    def run(self):
+
+        version = self.current_db_version
+        if version > '1.2.8':
+            self.drop_views()
+
+    def drop_views(self):
+        drop_sql = "{}../../ordinary_data/views/drop_views.sql".format(self.delta_dir)
+        pg = self.pg_service
+
+        print()
+        self.write_message("Dropping views")
+
+        # Compose command
+        cmd = 'psql "service = {pg}" -f {drop_sql}'.format(pg=pg, drop_sql=drop_sql)
+        self.write_message(cmd)
+
+        # Execute command
+        os.system(cmd)
+        self.write_message("Dropping views: done")
