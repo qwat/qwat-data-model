f4aa3f7d984caf98e94b9362ce06535d72c233bc
Sylvain Beorchia <sylvain.beorchia@oslandia.com>
2017-10-27 13:18:27 +0200

    Fixing PGSERVICE state before launching commands

--- update/delta/post-all.py
+++ update/delta/post-all.py
@@ -13,9 +13,9 @@ class RecreateViewsAndFunctions(DeltaPy):
         """
         self.write_message("Reloading views and functions")
 
-        export_db = "export PGSERVICE=qwat_test"
-        views_sh = "SRID=21781 {}../../ordinary_data/views/rewrite_views.sh".format(self.delta_dir)
-        functions_sh = "SRID=21781 {}../../ordinary_data/functions/rewrite_functions.sh".format(self.delta_dir)
+        #export_db = "export PGSERVICE=qwat_test"
+        views_sh = "PGSERVICE=qwat_test SRID=21781 {}../../ordinary_data/views/rewrite_views.sh".format(self.delta_dir)
+        functions_sh = "PGSERVICE=qwat_test SRID=21781 {}../../ordinary_data/functions/rewrite_functions.sh".format(self.delta_dir)
 
         # Execute commands
         os.system(export_db)
