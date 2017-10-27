55aad51202e89f9bbba3c9e81eda116a8ba8c70e
Sylvain Beorchia <sylvain.beorchia@oslandia.com>
2017-10-27 12:37:21 +0200

    Set DB before recreating views and functions

--- update/delta/post-all.py
+++ update/delta/post-all.py
@@ -13,9 +13,11 @@ class RecreateViewsAndFunctions(DeltaPy):
         """
         self.write_message("Reloading views and functions")
 
+        export_db = "export PGSERVICE=qwat_test"
         views_sh = "SRID=21781 {}../../ordinary_data/views/rewrite_views.sh".format(self.delta_dir)
         functions_sh = "SRID=21781 {}../../ordinary_data/functions/rewrite_functions.sh".format(self.delta_dir)
 
         # Execute commands
+        os.system(export_db)
         os.system(views_sh)
         os.system(functions_sh)
