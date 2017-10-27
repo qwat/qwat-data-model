3ff0d61670209ad649de4418f6127dccc95d1a5e
Sylvain Beorchia <sylvain.beorchia@oslandia.com>
2017-10-27 13:26:59 +0200

    Cleaning post-all file

--- update/delta/post-all.py
+++ update/delta/post-all.py
@@ -6,14 +6,9 @@ class RecreateViewsAndFunctions(DeltaPy):
     def run(self):
 
         delta_dir = self.delta_dir
-        """
-        SRID=$SRID ./ordinary_data/views/rewrite_views.sh
-        SRID=$SRID ./ordinary_data/functions/rewrite_functions.sh
-        cd $DIR
-        """
+
         self.write_message("Reloading views and functions")
 
-        #export_db = "export PGSERVICE=qwat_test"
         views_sh = "PGSERVICE=qwat_test SRID=21781 {}../../ordinary_data/views/rewrite_views.sh".format(self.delta_dir)
         functions_sh = "PGSERVICE=qwat_test SRID=21781 {}../../ordinary_data/functions/rewrite_functions.sh".format(self.delta_dir)
 
