3456d501e3454349cf81c943118b2b49565a5c79
Sylvain Beorchia <sylvain.beorchia@oslandia.com>
2017-11-03 10:47:40 +0100

    Add comment + Removed useless scripts

--- update/delta/pre-all.py
+++ update/delta/pre-all.py
@@ -6,6 +6,11 @@ class DropViews(DeltaPy):
     def run(self):
 
         version = self.current_db_version
+
+        # This file is played before appliying any delta file.
+        # If you have to do or NOT to do some specific work, you must do it there
+        # For example, the 1.2.8 version delta contain some specific actions wich require to drop the views before playing them;
+        # But we have to drop them only if the current version is 1.2.8 or superior
         if version > '1.2.8':
             self.drop_views()
 
