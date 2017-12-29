1cd5d40530fb48e77226311152e2750cc2d66a3c
Denis Rouzaud <denis.rouzaud@gmail.com>
2017-12-29 16:41:19 -0400

    adds captage, hydrante, reservoire, station pompage, station traitement

--- extensions/ch_vd_sire/drop_views.sql
+++ extensions/ch_vd_sire/drop_views.sql
@@ -2,3 +2,8 @@
 DROP VIEW IF EXISTS qwat_ch_vd_sire.conduite;
 DROP VIEW IF EXISTS qwat_ch_vd_sire.vanne_clapet;
 DROP VIEW IF EXISTS qwat_ch_vd_sire.regulation_pression;
+DROP VIEW IF EXISTS qwat_ch_vd_sire.station_pompage;
+DROP VIEW IF EXISTS qwat_ch_vd_sire.station_traitement;
+DROP VIEW IF EXISTS qwat_ch_vd_sire.reservoir;
+DROP VIEW IF EXISTS qwat_ch_vd_sire.captage;
+DROP VIEW IF EXISTS qwat_ch_vd_sire.hydrante;
--- extensions/ch_vd_sire/insert_views.sh
+++ extensions/ch_vd_sire/insert_views.sh
@@ -8,3 +8,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 psql -v ON_ERROR_STOP=1 -v SRID=$SRID -f ${DIR}/views/conduite.sql
 psql -v ON_ERROR_STOP=1 -v SRID=$SRID -f ${DIR}/views/vanne_clapet.sql
 psql -v ON_ERROR_STOP=1 -v SRID=$SRID -f ${DIR}/views/regulation_pression.sql
+psql -v ON_ERROR_STOP=1 -v SRID=$SRID -f ${DIR}/views/station_pompage.sql
+psql -v ON_ERROR_STOP=1 -v SRID=$SRID -f ${DIR}/views/station_traitement.sql
+psql -v ON_ERROR_STOP=1 -v SRID=$SRID -f ${DIR}/views/reservoir.sql
+psql -v ON_ERROR_STOP=1 -v SRID=$SRID -f ${DIR}/views/captage.sql
+psql -v ON_ERROR_STOP=1 -v SRID=$SRID -f ${DIR}/views/hydrante.sql
