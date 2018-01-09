1eb3e070d5a515c9d0ed310f40253c52044ec156
Sylvain Beorchia <sylvain.beorchia@oslandia.com>
2018-01-09 13:54:49 +0100

    Fix missing constraint for valves in DELTAs

---
+++ update/delta/delta_1.3.2_fix_valve_missing_constraints.sql
@@ -0,0 +1,6 @@
+
+
+-- Missing valve constraints
+
+ALTER TABLE qwat_od.valve ADD CONSTRAINT chk_valve_altitude_obj_ref CHECK (fk_object_reference IS NOT NULL OR altitude IS NULL );
+ALTER TABLE qwat_od.valve ADD CONSTRAINT chk_valve_altitude_precisionalti CHECK (fk_precisionalti IS NOT NULL OR altitude IS NULL );
