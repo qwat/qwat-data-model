d3dbec4b6f39e7789947e40e1b649690d9959f61
Sylvain Beorchia <sylvain.beorchia@oslandia.com>
2018-01-09 14:15:43 +0100

    Removing -i option to PUM

--- update/delta/delta_1.3.2_fix_valve_missing_constraints.sql
+++ update/delta/delta_1.3.2_fix_valve_missing_constraints.sql
@@ -2,5 +2,5 @@
 
 -- Missing valve constraints
 
-ALTER TABLE qwat_od.valve ADD CONSTRAINT chk_valve_altitude_obj_ref CHECK (fk_object_reference IS NOT NULL OR altitude IS NULL );
-ALTER TABLE qwat_od.valve ADD CONSTRAINT chk_valve_altitude_precisionalti CHECK (fk_precisionalti IS NOT NULL OR altitude IS NULL );
+-- ALTER TABLE qwat_od.valve ADD CONSTRAINT chk_valve_altitude_obj_ref CHECK (fk_object_reference IS NOT NULL OR altitude IS NULL );
+-- ALTER TABLE qwat_od.valve ADD CONSTRAINT chk_valve_altitude_precisionalti CHECK (fk_precisionalti IS NOT NULL OR altitude IS NULL );
