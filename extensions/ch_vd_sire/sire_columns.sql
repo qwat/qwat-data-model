08f0fcaba331595d94a9bfc75da37b4df4ff9c10
Denis Rouzaud <denis.rouzaud@gmail.com>
2018-02-12 13:47:38 -0400

    add missing column

--- extensions/ch_vd_sire/sire_columns.sql
+++ extensions/ch_vd_sire/sire_columns.sql
@@ -13,3 +13,4 @@ ALTER TABLE qwat_od.network_element ADD COLUMN qwat_ext_ch_vd_sire_remarque text
 
 ALTER TABLE qwat_od.valve ADD COLUMN qwat_ext_ch_vd_sire_etat_exploitation smallint;
 ALTER TABLE qwat_od.valve ADD COLUMN qwat_ext_ch_vd_sire_remarque text;
+ALTER TABLE qwat_od.valve ADD COLUMN qwat_ext_ch_vd_sire_adesafecter smallint;
