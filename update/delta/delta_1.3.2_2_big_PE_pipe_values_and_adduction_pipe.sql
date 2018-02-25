c0ccfbe1f4785e5e032dd8ae526238c07324ef5d
Tudor Bărăscu <tudor.barascu@qtibia.ro>
2018-02-25 16:12:24 +0200

    added delta for big PE diameter value + adduction

---
+++ update/delta/delta_1.3.2_2_big_PE_pipe_values_and_adduction_pipe.sql
@@ -0,0 +1,68 @@
+-- Add adduction pipe value
+INSERT INTO qwat_vl.pipe_function (id,schema_visible,major,value_fr,value_ro) VALUES (4104, true, true, 'Conduite d''adduction',      'Conductă de aducțiune'        );
+
+
+-- Add big PE pipes values
+INSERT INTO qwat_vl.pipe_material (id, vl_active, short_en, value_en, short_fr, value_fr, short_ro, value_ro, diameter, diameter_nominal, diameter_internal, diameter_external, code_sire, pressure_nominal, sdr, wall_thickness) VALUES (5528, true, '', '', 'PE', 'Polyéthylène 100', 'PE', 'Polietilenă 100', '800', NULL, 654.6, 800, 10, 16, 11, 72.7);
+
+INSERT INTO qwat_vl.pipe_material (id, vl_active, short_en, value_en, short_fr, value_fr, short_ro, value_ro, diameter, diameter_nominal, diameter_internal, diameter_external, code_sire, pressure_nominal, sdr, wall_thickness) VALUES (5529, true, '', '', 'PE', 'Polyéthylène 100', 'PE', 'Polietilenă 100', '900', NULL, 736.4, 900, 10, 16, 11, 81.8);
+
+INSERT INTO qwat_vl.pipe_material (id, vl_active, short_en, value_en, short_fr, value_fr, short_ro, value_ro, diameter, diameter_nominal, diameter_internal, diameter_external, code_sire, pressure_nominal, sdr, wall_thickness) VALUES (5530, true, '', '', 'PE', 'Polyéthylène 100', 'PE', 'Polietilenă 100', '1000', NULL, 818.2, 1000, 10, 16, 11, 90.9);
+
+INSERT INTO qwat_vl.pipe_material (id, vl_active, short_en, value_en, short_fr, value_fr, short_ro, value_ro, diameter, diameter_nominal, diameter_internal, diameter_external, code_sire, pressure_nominal, sdr, wall_thickness) VALUES (5903, true, '', '', '', 'Béton précontraint', 'PREMO', 'Beton precomprimat', '1000', 1000, NULL, NULL, NULL, NULL, NULL, NULL);
+
+INSERT INTO qwat_vl.pipe_material (id, vl_active, short_en, value_en, short_fr, value_fr, short_ro, value_ro, diameter, diameter_nominal, diameter_internal, diameter_external, code_sire, pressure_nominal, sdr, wall_thickness) VALUES (5000, true, '', '', 'PE', 'Polyéthylène 100', 'PE', 'Polietilenă 100', '800', NULL, 738.8, 800.000, 10, 6, 26, 30.6);
+
+INSERT INTO qwat_vl.pipe_material (id, vl_active, short_en, value_en, short_fr, value_fr, short_ro, value_ro, diameter, diameter_nominal, diameter_internal, diameter_external, code_sire, pressure_nominal, sdr, wall_thickness) VALUES (5001, true, '', '', 'PE', 'Polyéthylène 100', 'PE', 'Polietilenă 100', '900', NULL, 831.2, 900.000, 10, 6, 26, 34.4);
+
+INSERT INTO qwat_vl.pipe_material (id, vl_active, short_en, value_en, short_fr, value_fr, short_ro, value_ro, diameter, diameter_nominal, diameter_internal, diameter_external, code_sire, pressure_nominal, sdr, wall_thickness) VALUES (5002, true, '', '', 'PE', 'Polyéthylène 100', 'PE', 'Polietilenă 100', '1000', NULL, 923.6, 1000.000, 10, 6, 26, 38.2);
+
+INSERT INTO qwat_vl.pipe_material (id, vl_active, short_en, value_en, short_fr, value_fr, short_ro, value_ro, diameter, diameter_nominal, diameter_internal, diameter_external, code_sire, pressure_nominal, sdr, wall_thickness) VALUES (5003, true, '', '', 'PE', 'Polyéthylène 100', 'PE', 'Polietilenă 100', '1200', NULL, 1106.2, 1200.000, 10, 6, 26, 46.9);
+
+INSERT INTO qwat_vl.pipe_material (id, vl_active, short_en, value_en, short_fr, value_fr, short_ro, value_ro, diameter, diameter_nominal, diameter_internal, diameter_external, code_sire, pressure_nominal, sdr, wall_thickness) VALUES (5004, true, '', '', 'PE', 'Polyéthylène 100', 'PE', 'Polietilenă 100', '1400', NULL, 1293, 1400.000, 10, 6, 26, 53.5);
+
+INSERT INTO qwat_vl.pipe_material (id, vl_active, short_en, value_en, short_fr, value_fr, short_ro, value_ro, diameter, diameter_nominal, diameter_internal, diameter_external, code_sire, pressure_nominal, sdr, wall_thickness) VALUES (5005, true, '', '', 'PE', 'Polyéthylène 100', 'PE', 'Polietilenă 100', '1600', NULL, 1477.6, 1600.000, 10, 6, 26, 61.2);
+
+INSERT INTO qwat_vl.pipe_material (id, vl_active, short_en, value_en, short_fr, value_fr, short_ro, value_ro, diameter, diameter_nominal, diameter_internal, diameter_external, code_sire, pressure_nominal, sdr, wall_thickness) VALUES (5006, true, '', '', 'PE', 'Polyéthylène 100', 'PE', 'Polietilenă 100', '1800', NULL, 1662.4, 1800.000, 10, 6, 26, 68.8);
+
+INSERT INTO qwat_vl.pipe_material (id, vl_active, short_en, value_en, short_fr, value_fr, short_ro, value_ro, diameter, diameter_nominal, diameter_internal, diameter_external, code_sire, pressure_nominal, sdr, wall_thickness) VALUES (5007, true, '', '', 'PE', 'Polyéthylène 100', 'PE', 'Polietilenă 100', '2000', NULL, 1847.2, 2000.000, 10, 6, 26, 76.4);
+
+INSERT INTO qwat_vl.pipe_material (id, vl_active, short_en, value_en, short_fr, value_fr, short_ro, value_ro, diameter, diameter_nominal, diameter_internal, diameter_external, code_sire, pressure_nominal, sdr, wall_thickness) VALUES (5008, true, '', '', 'PE', 'Polyéthylène 100', 'PE', 'Polietilenă 100', '2250', NULL, 2078, 2250.000, 10, 6, 26, 86);
+
+INSERT INTO qwat_vl.pipe_material (id, vl_active, short_en, value_en, short_fr, value_fr, short_ro, value_ro, diameter, diameter_nominal, diameter_internal, diameter_external, code_sire, pressure_nominal, sdr, wall_thickness) VALUES (5009, true, '', '', 'PE', 'Polyéthylène 100', 'PE', 'Polietilenă 100', '2500', NULL, 2308.8, 2500.000, 10, 6, 26, 95.6);
+
+INSERT INTO qwat_vl.pipe_material (id, vl_active, short_en, value_en, short_fr, value_fr, short_ro, value_ro, diameter, diameter_nominal, diameter_internal, diameter_external, code_sire, pressure_nominal, sdr, wall_thickness) VALUES (5539, true, '', '', 'PE', 'Polyéthylène 100', 'PE', 'Polietilenă 100', '800', NULL, 723.8, 800, 10, 8, 21, 38.1);
+
+INSERT INTO qwat_vl.pipe_material (id, vl_active, short_en, value_en, short_fr, value_fr, short_ro, value_ro, diameter, diameter_nominal, diameter_internal, diameter_external, code_sire, pressure_nominal, sdr, wall_thickness) VALUES (5540, true, '', '', 'PE', 'Polyéthylène 100', 'PE', 'Polietilenă 100', '900', NULL, 814.2, 900, 10, 8, 21, 42.9);
+
+INSERT INTO qwat_vl.pipe_material (id, vl_active, short_en, value_en, short_fr, value_fr, short_ro, value_ro, diameter, diameter_nominal, diameter_internal, diameter_external, code_sire, pressure_nominal, sdr, wall_thickness) VALUES (5541, true, '', '', 'PE', 'Polyéthylène 100', 'PE', 'Polietilenă 100', '1000', NULL, 904.6, 1000, 10, 8, 21, 47.7);
+
+INSERT INTO qwat_vl.pipe_material (id, vl_active, short_en, value_en, short_fr, value_fr, short_ro, value_ro, diameter, diameter_nominal, diameter_internal, diameter_external, code_sire, pressure_nominal, sdr, wall_thickness) VALUES (5542, true, '', '', 'PE', 'Polyéthylène 100', 'PE', 'Polietilenă 100', '1200', NULL, 1085.6, 1200, 10, 8, 21, 57.2);
+
+INSERT INTO qwat_vl.pipe_material (id, vl_active, short_en, value_en, short_fr, value_fr, short_ro, value_ro, diameter, diameter_nominal, diameter_internal, diameter_external, code_sire, pressure_nominal, sdr, wall_thickness) VALUES (5543, true, '', '', 'PE', 'Polyéthylène 100', 'PE', 'Polietilenă 100', '1400', NULL, 1266.6, 1400, 10, 8, 21, 66.7);
+
+INSERT INTO qwat_vl.pipe_material (id, vl_active, short_en, value_en, short_fr, value_fr, short_ro, value_ro, diameter, diameter_nominal, diameter_internal, diameter_external, code_sire, pressure_nominal, sdr, wall_thickness) VALUES (5544, true, '', '', 'PE', 'Polyéthylène 100', 'PE', 'Polietilenă 100', '1600', NULL, 1447.6, 1600, 10, 8, 21, 76.2);
+
+INSERT INTO qwat_vl.pipe_material (id, vl_active, short_en, value_en, short_fr, value_fr, short_ro, value_ro, diameter, diameter_nominal, diameter_internal, diameter_external, code_sire, pressure_nominal, sdr, wall_thickness) VALUES (5545, true, '', '', 'PE', 'Polyéthylène 100', 'PE', 'Polietilenă 100', '1800', NULL, 1628.4, 1800, 10, 8, 21, 85.8);
+
+INSERT INTO qwat_vl.pipe_material (id, vl_active, short_en, value_en, short_fr, value_fr, short_ro, value_ro, diameter, diameter_nominal, diameter_internal, diameter_external, code_sire, pressure_nominal, sdr, wall_thickness) VALUES (5546, true, '', '', 'PE', 'Polyéthylène 100', 'PE', 'Polietilenă 100', '2000', NULL, 1809.4, 2000, 10, 8, 21, 95.3);
+
+INSERT INTO qwat_vl.pipe_material (id, vl_active, short_en, value_en, short_fr, value_fr, short_ro, value_ro, diameter, diameter_nominal, diameter_internal, diameter_external, code_sire, pressure_nominal, sdr, wall_thickness) VALUES (5547, true, '', '', 'PE', 'Polyéthylène 100', 'PE', 'Polietilenă 100', '2250', NULL, 2035.6, 2250, 10, 8, 21, 107.2);
+
+INSERT INTO qwat_vl.pipe_material (id, vl_active, short_en, value_en, short_fr, value_fr, short_ro, value_ro, diameter, diameter_nominal, diameter_internal, diameter_external, code_sire, pressure_nominal, sdr, wall_thickness) VALUES (5548, true, '', '', 'PE', 'Polyéthylène 100', 'PE', 'Polietilenă 100', '2500', NULL, 2261.8, 2500, 10, 8, 21, 119.1);
+
+INSERT INTO qwat_vl.pipe_material (id, vl_active, short_en, value_en, short_fr, value_fr, short_ro, value_ro, diameter, diameter_nominal, diameter_internal, diameter_external, code_sire, pressure_nominal, sdr, wall_thickness) VALUES (5531, true, '', '', 'PE', 'Polyéthylène 100', 'PE', 'Polietilenă 100', '800', NULL, 705.2, 800, 10, 10, 17, 47.4);
+
+INSERT INTO qwat_vl.pipe_material (id, vl_active, short_en, value_en, short_fr, value_fr, short_ro, value_ro, diameter, diameter_nominal, diameter_internal, diameter_external, code_sire, pressure_nominal, sdr, wall_thickness) VALUES (5532, true, '', '', 'PE', 'Polyéthylène 100', 'PE', 'Polietilenă 100', '900', NULL, 793.4, 900, 10, 10, 17, 53.3);
+
+INSERT INTO qwat_vl.pipe_material (id, vl_active, short_en, value_en, short_fr, value_fr, short_ro, value_ro, diameter, diameter_nominal, diameter_internal, diameter_external, code_sire, pressure_nominal, sdr, wall_thickness) VALUES (5533, true, '', '', 'PE', 'Polyéthylène 100', 'PE', 'Polietilenă 100', '1000', NULL, 881.4, 1000, 10, 10, 17, 59.3);
+
+INSERT INTO qwat_vl.pipe_material (id, vl_active, short_en, value_en, short_fr, value_fr, short_ro, value_ro, diameter, diameter_nominal, diameter_internal, diameter_external, code_sire, pressure_nominal, sdr, wall_thickness) VALUES (5534, true, '', '', 'PE', 'Polyéthylène 100', 'PE', 'Polietilenă 100', '1200', NULL, 1058.8, 1200, 10, 10, 17, 70.6);
+
+INSERT INTO qwat_vl.pipe_material (id, vl_active, short_en, value_en, short_fr, value_fr, short_ro, value_ro, diameter, diameter_nominal, diameter_internal, diameter_external, code_sire, pressure_nominal, sdr, wall_thickness) VALUES (5535, true, '', '', 'PE', 'Polyéthylène 100', 'PE', 'Polietilenă 100', '1400', NULL, 1235.2, 1400, 10, 10, 17, 82.4);
+
+INSERT INTO qwat_vl.pipe_material (id, vl_active, short_en, value_en, short_fr, value_fr, short_ro, value_ro, diameter, diameter_nominal, diameter_internal, diameter_external, code_sire, pressure_nominal, sdr, wall_thickness) VALUES (5536, true, '', '', 'PE', 'Polyéthylène 100', 'PE', 'Polietilenă 100', '1600', NULL, 1411.8, 1600, 10, 10, 17, 94.1);
+
+INSERT INTO qwat_vl.pipe_material (id, vl_active, short_en, value_en, short_fr, value_fr, short_ro, value_ro, diameter, diameter_nominal, diameter_internal, diameter_external, code_sire, pressure_nominal, sdr, wall_thickness) VALUES (5537, true, '', '', 'PE', 'Polyéthylène 100', 'PE', 'Polietilenă 100', '1800', NULL, 1586.8, 1800, 10, 10, 17, 106.6);
+
+INSERT INTO qwat_vl.pipe_material (id, vl_active, short_en, value_en, short_fr, value_fr, short_ro, value_ro, diameter, diameter_nominal, diameter_internal, diameter_external, code_sire, pressure_nominal, sdr, wall_thickness) VALUES (5538, true, '', '', 'PE', 'Polyéthylène 100', 'PE', 'Polietilenă 100', '2000', NULL, 1763.2, 2000, 10, 10, 17, 118.4);
