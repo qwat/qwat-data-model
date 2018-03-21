9d6dc8d6111d9d2bc8518c0c4e862af8e46bf120
Éric Lemoine <eric.lemoine@oslandia.com>
2018-03-21 14:09:41 +0100

    Add post-all.py and pre-all.py scripts for ch_vd_sire

---
+++ extensions/ch_vd_sire/delta/pre-all.py
@@ -0,0 +1,15 @@
+import os
+from pum.core.deltapy import DeltaPy
+
+
+class DropViews(DeltaPy):
+
+    def run(self):
+        self.write_message('Dropping ch_vd_sire views')
+
+        drop_sql = os.path.join(self.delta_dir, '..', 'drop_views.sql')
+        cmd = 'psql service={} -f {}'.format(self.pg_service, drop_sql)
+        self.write_message(cmd)
+        os.system(cmd)
+
+        self.write_message('Dropping ch_vd_sire views: done')
--- extensions/ch_vd_sire/rewrite_views.sh
+++ extensions/ch_vd_sire/rewrite_views.sh
@@ -6,3 +6,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 psql service=${PGSERVICE} -v ON_ERROR_STOP=1 -f ${DIR}/drop_views.sql
 PGSERVICE=${PGSERVICE} SRID=${SRID} ${DIR}/insert_views.sh
+
+exit 0
