3bd1dd61576102009aafab80efbef91b42302513
Loïc Bartoletti <loic.bartoletti@oslandia.com>
2018-08-23 07:57:44 +0200

    RO translation

--- update/delta/delta_1.3.3_005_add_compteurdechantier_part_type.sql
+++ update/delta/delta_1.3.3_005_add_compteurdechantier_part_type.sql
@@ -1,1 +1,1 @@
-INSERT INTO qwat_vl.part_type (id, value_en, value_fr, vl_active) VALUES (9220, 'site water meter', 'compteur de chantier', true)
+INSERT INTO qwat_vl.part_type (id, value_en, value_fr, value_ro, vl_active) VALUES (9220, 'site water meter', 'compteur de chantier', 'contor de construcții', true)
