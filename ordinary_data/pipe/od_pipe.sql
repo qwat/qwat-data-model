ae933714dd3a4c5086c30bb879eac769ee39ac5b
Tudor Bărăscu <tudor.barascu@qtibia.ro>
2018-11-10 12:38:59 +0200

    add delta for year constraints changes

--- ordinary_data/pipe/od_pipe.sql
+++ ordinary_data/pipe/od_pipe.sql
@@ -54,14 +54,14 @@ ALTER TABLE qwat_od.pipe ADD CONSTRAINT pipe_fk_status         FOREIGN KEY (fk_s
 ALTER TABLE qwat_od.pipe ADD CONSTRAINT pipe_fk_watertype      FOREIGN KEY (fk_watertype)      REFERENCES qwat_vl.watertype(id)          MATCH FULL; CREATE INDEX fki_pipe_fk_watertype     ON qwat_od.pipe(fk_watertype);
 ALTER TABLE qwat_od.pipe ADD CONSTRAINT pipe_fk_folder         FOREIGN KEY (fk_folder)         REFERENCES qwat_od.folder(id)             MATCH FULL; CREATE INDEX fki_pipe_fk_folder        ON qwat_od.pipe(fk_folder);
 
-ALTER TABLE qwat_od.pipe ADD CONSTRAINT ck_pipe_year CHECK (year IS NULL OR year > 1800 AND (year <= EXTRACT(YEAR FROM NOW()) OR fk_status = 1306 AND year - 50 <= EXTRACT(YEAR FROM NOW())));
-COMMENT ON CONSTRAINT ck_pipe_year ON qwat_od.pipe IS 'Year when the pipe was installed should be either NULL - not filled OR between 1800 and the current year; if the pipe has the project status (fk_status = 1306) then the year can be between 1800 and 50 years from now';
+ALTER TABLE qwat_od.pipe ADD CONSTRAINT pipe_year_check CHECK (year IS NULL OR year > 1800 AND (year <= EXTRACT(YEAR FROM NOW()) OR fk_status = 1306 AND year - 50 <= EXTRACT(YEAR FROM NOW())));
+COMMENT ON CONSTRAINT pipe_year_check ON qwat_od.pipe IS 'Year when the pipe was installed should be either NULL - not filled OR between 1800 and the current year; if the pipe has the project status (fk_status = 1306) then the year can be between 1800 and 50 years from now';
 
-ALTER TABLE qwat_od.pipe ADD CONSTRAINT ck_pipe_year_rehabilitation CHECK (year_rehabilitation IS NULL OR year_rehabilitation > 1800 AND year_rehabilitation <= EXTRACT(YEAR FROM NOW()));
-COMMENT ON CONSTRAINT ck_pipe_year_rehabilitation ON qwat_od.pipe IS 'Pipe rehabilitation year should be between 1800 and the current year or NULL/not filled';
+ALTER TABLE qwat_od.pipe ADD CONSTRAINT pipe_year_rehabilitation_check CHECK (year_rehabilitation IS NULL OR year_rehabilitation > 1800 AND year_rehabilitation <= EXTRACT(YEAR FROM NOW()));
+COMMENT ON CONSTRAINT pipe_year_rehabilitation_check ON qwat_od.pipe IS 'Pipe rehabilitation year should be between 1800 and the current year or NULL/not filled';
 
-ALTER TABLE qwat_od.pipe ADD CONSTRAINT ck_pipe_year_end CHECK (year_end IS NULL OR year_end > 1800 AND year_end <= EXTRACT(YEAR FROM NOW()));
-COMMENT ON CONSTRAINT ck_pipe_year_end ON qwat_od.pipe IS 'Year when the pipe was shut down should be between 1800 and the current year or NULL/not filled';
+ALTER TABLE qwat_od.pipe ADD CONSTRAINT pipe_year_end_check CHECK (year_end IS NULL OR year_end > 1800 AND year_end <= EXTRACT(YEAR FROM NOW()));
+COMMENT ON CONSTRAINT pipe_year_end_check ON qwat_od.pipe IS 'Year when the pipe was shut down should be between 1800 and the current year or NULL/not filled';
 
 /*----------------!!!---!!!----------------*/
 /* Trigger for tunnel_or_bridge */
