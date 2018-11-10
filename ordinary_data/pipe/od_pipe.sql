90d4fb30da2ecb6c09ef2c1a5e9c31ca1195a25d
Tudor Bărăscu <tudor.barascu@qtibia.ro>
2018-11-10 12:10:21 +0200

    fix year check considering fk_status + better comments

--- ordinary_data/pipe/od_pipe.sql
+++ ordinary_data/pipe/od_pipe.sql
@@ -31,9 +31,9 @@ ALTER TABLE qwat_od.pipe ADD COLUMN remark           text         ;
 ALTER TABLE qwat_od.pipe ADD COLUMN _valve_count     smallint default NULL;
 ALTER TABLE qwat_od.pipe ADD COLUMN _valve_closed    boolean default NULL;
 
-COMMENT ON COLUMN qwat_od.pipe.year IS 'Represents the year when the pipe was installed.';
-COMMENT ON COLUMN qwat_od.pipe.year_rehabilitation IS 'Represents the year when the pipe was rehabilitated.';
-COMMENT ON COLUMN qwat_od.pipe.year_end IS 'Represents the year when the pipe was shut down.';
+COMMENT ON COLUMN qwat_od.pipe.year IS 'Represents the year when the pipe was installed. It can be NULL (not filled), between 1800 and the current year. If the pipe has project status then the maximum value of the year can be 50 years from the current year.';
+COMMENT ON COLUMN qwat_od.pipe.year_rehabilitation IS 'Represents the year when the pipe was rehabilitated. It can be NULL (not filled) or between 1800 and the current year.';
+COMMENT ON COLUMN qwat_od.pipe.year_end IS 'Represents the year when the pipe was shut down. It can be NULL (not filled) or between 1800 and the current year.';
 
 /* schema view */
 DO $$ BEGIN PERFORM qwat_sys.fn_enable_schemavisible( 'pipe', 'pipe_function', 'fk_function' ); END $$;
@@ -53,14 +53,15 @@ ALTER TABLE qwat_od.pipe ADD CONSTRAINT pipe_fk_protection     FOREIGN KEY (fk_p
 ALTER TABLE qwat_od.pipe ADD CONSTRAINT pipe_fk_status         FOREIGN KEY (fk_status)         REFERENCES qwat_vl.status(id)             MATCH FULL; CREATE INDEX fki_pipe_fk_status        ON qwat_od.pipe(fk_status);
 ALTER TABLE qwat_od.pipe ADD CONSTRAINT pipe_fk_watertype      FOREIGN KEY (fk_watertype)      REFERENCES qwat_vl.watertype(id)          MATCH FULL; CREATE INDEX fki_pipe_fk_watertype     ON qwat_od.pipe(fk_watertype);
 ALTER TABLE qwat_od.pipe ADD CONSTRAINT pipe_fk_folder         FOREIGN KEY (fk_folder)         REFERENCES qwat_od.folder(id)             MATCH FULL; CREATE INDEX fki_pipe_fk_folder        ON qwat_od.pipe(fk_folder);
-ALTER TABLE qwat_od.pipe ADD CONSTRAINT ck_pipe_year CHECK
-(year IS NULL OR year > 1800 AND year <= EXTRACT(YEAR FROM NOW()) OR 
-(fk_status = 1306 AND (year IS NULL OR year >=  EXTRACT(YEAR FROM NOW()) AND year < 2100)));
-COMMENT ON CONSTRAINT ck_pipe_year ON qwat_od.pipe IS 'Year when the pipe was installed should be between 1800 and the current year or unknown. If the pipe is in a project then the year column shows when the pipe is to be installed which is between the current year and 2100';
+
+ALTER TABLE qwat_od.pipe ADD CONSTRAINT ck_pipe_year CHECK (year IS NULL OR year > 1800 AND (year <= EXTRACT(YEAR FROM NOW()) OR fk_status = 1306 AND year - 50 <= EXTRACT(YEAR FROM NOW())));
+COMMENT ON CONSTRAINT ck_pipe_year ON qwat_od.pipe IS 'Year when the pipe was installed should be either NULL - not filled OR between 1800 and the current year; if the pipe has the project status (fk_status = 1306) then the year can be between 1800 and 50 years from now';
+
 ALTER TABLE qwat_od.pipe ADD CONSTRAINT ck_pipe_year_rehabilitation CHECK (year_rehabilitation IS NULL OR year_rehabilitation > 1800 AND year_rehabilitation <= EXTRACT(YEAR FROM NOW()));
-COMMENT ON CONSTRAINT ck_pipe_year_rehabilitation ON qwat_od.pipe IS 'Pipe rehabilitation year should be between 1800 and the current year or unknown.';
+COMMENT ON CONSTRAINT ck_pipe_year_rehabilitation ON qwat_od.pipe IS 'Pipe rehabilitation year should be between 1800 and the current year or NULL/not filled';
+
 ALTER TABLE qwat_od.pipe ADD CONSTRAINT ck_pipe_year_end CHECK (year_end IS NULL OR year_end > 1800 AND year_end <= EXTRACT(YEAR FROM NOW()));
-COMMENT ON CONSTRAINT ck_pipe_year_end ON qwat_od.pipe IS 'Year when the pipe was shut down should be between 1800 and the current year or unknown';
+COMMENT ON CONSTRAINT ck_pipe_year_end ON qwat_od.pipe IS 'Year when the pipe was shut down should be between 1800 and the current year or NULL/not filled';
 
 /*----------------!!!---!!!----------------*/
 /* Trigger for tunnel_or_bridge */
