7e2ddbd779a63d802a2725370477fa2ab4992e3f
Tudor Bărăscu <tudor.barascu@qtibia.ro>
2018-11-14 20:41:45 +0200

    also drop the useless fkey constraint

--- update/delta/delta_1.3.3_014_add_surveypoints_sequence.sql
+++ update/delta/delta_1.3.3_014_add_surveypoints_sequence.sql
@@ -1,2 +1,3 @@
 CREATE SEQUENCE qwat_od.surveypoint_id_seq;
 ALTER TABLE qwat_od.surveypoint ALTER COLUMN id SET DEFAULT nextval('qwat_od.surveypoint_id_seq'::regclass);
+ALTER TABLE qwat_od.surveypoint DROP CONSTRAINT surveypoint_id_fkey;
