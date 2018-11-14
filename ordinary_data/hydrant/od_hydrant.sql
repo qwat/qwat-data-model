b6ab84959223c0604aab521bec8e9a0034f138e7
Tudor Bărăscu <tudor.barascu@qtibia.ro>
2018-11-14 13:50:26 +0200

    add hydrant parts comments

--- ordinary_data/hydrant/od_hydrant.sql
+++ ordinary_data/hydrant/od_hydrant.sql
@@ -36,3 +36,13 @@ ALTER TABLE qwat_od.hydrant ADD CONSTRAINT hydrant_fk_output    FOREIGN KEY (fk_
 ALTER table qwat_od.hydrant ADD CONSTRAINT above_ground_hydrant_has_2_parts_check check (
 (fk_model_sup IS NOT NULL OR underground IS TRUE) -- If the hydrant is above ground fk_model_sup should be filled
 );
+COMMENT ON CONSTRAINT above_ground_hydrant_has_2_parts_check ON qwat_od.hydrant
+    IS 'Checks that the models for the above and underground parts of the above the ground hydrant are both filled.';
+
+/* COMMENTS */
+
+COMMENT ON COLUMN qwat_od.hydrant.fk_model_sup
+    IS 'Above ground part model (for above ground hydrants). For the underground hydrant it should not exist.';
+
+COMMENT ON COLUMN qwat_od.hydrant.fk_model_inf
+    IS 'Underground part model (for above ground hydrants). For the underground hydrant it represents the actual hydrant model.';
--- update/delta/delta_1.3.3_013_allow_single_part_underground_hydrants.sql
+++ update/delta/delta_1.3.3_013_allow_single_part_underground_hydrants.sql
@@ -2,3 +2,12 @@ ALTER TABLE qwat_od.hydrant ALTER COLUMN fk_model_sup DROP NOT NULL;
 ALTER table qwat_od.hydrant ADD CONSTRAINT above_ground_hydrant_has_2_parts_check check (
 (fk_model_sup IS NOT NULL OR underground IS TRUE) -- If the hydrant is above ground fk_model_sup should be filled
 );
+
+COMMENT ON COLUMN qwat_od.hydrant.fk_model_sup
+    IS 'Above ground part model (for above ground hydrants). For the underground hydrant it should not exist.';
+
+COMMENT ON COLUMN qwat_od.hydrant.fk_model_inf
+    IS 'Underground part model (for above ground hydrants). For the underground hydrant it represents the actual hydrant model.';
+
+COMMENT ON CONSTRAINT above_ground_hydrant_has_2_parts_check ON qwat_od.hydrant
+    IS 'Checks that the models for the above and underground parts of the above the ground hydrant are both filled.';
