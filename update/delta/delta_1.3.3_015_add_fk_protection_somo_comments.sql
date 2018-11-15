ddb650686dc660b2d7374f0bab9b16af80feba4f
Tudor Bărăscu <tudor.barascu@qtibia.ro>
2018-11-15 12:52:25 +0200

    delta for comments

---
+++ update/delta/delta_1.3.3_015_add_fk_protection_somo_comments.sql
@@ -0,0 +1,2 @@
+UPDATE qwat_vl.pipe_protection SET description_ro = 'SOMO este o marcă dispărută a grupului Bouygues, nefolosită în Ro' WHERE id IN (4302,4304);
+UPDATE qwat_vl.pipe_protection SET description_fr = 'SOMO est une ancienne marque disparue du groupe Bouygues qui ne caractérisait pas vraiment un type de tuyau, même si, dans la mémoire collective des années 80-90, cela voulait plutôt désigner, à tort, les tuyaux PE-câbles', description_ro = 'SOMO este o marcă dispărută a grupului Bouygues, nefolosită în Ro' WHERE id = 4310;
