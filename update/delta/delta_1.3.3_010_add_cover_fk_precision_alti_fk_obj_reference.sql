37f8faace5664cf0c58c5a46bd26f20c56107916
Tudor Bărăscu <tudor.barascu@qtibia.ro>
2019-03-03 08:34:55 +0200

    Set fk_object_reference and fk_precisionalti "TO BE DETERMINED" for covers with existing altitude
    Follow-up to #188

--- update/delta/delta_1.3.3_010_add_cover_fk_precision_alti_fk_obj_reference.sql
+++ update/delta/delta_1.3.3_010_add_cover_fk_precision_alti_fk_obj_reference.sql
@@ -12,5 +12,8 @@ CREATE INDEX fki_cover_fk_precision     ON qwat_od.cover(fk_precision);
 ALTER TABLE qwat_od.cover ADD CONSTRAINT cover_fk_object_reference FOREIGN KEY (fk_object_reference) REFERENCES qwat_vl.object_reference(id) MATCH FULL;
 CREATE INDEX fki_cover_fk_object_reference ON qwat_od.cover(fk_object_reference);
 
+-- Set fk_object_reference and fk_precisionalti to "TO BE DETERMINED" for covers with existing altitude
+UPDATE qwat_od.cover SET fk_object_reference = 103, fk_precisionalti = 103 WHERE altitude IS NOT NULL;
+
 ALTER TABLE qwat_od.cover ADD CONSTRAINT chk_cover_altitude_obj_ref CHECK (fk_object_reference IS NOT NULL OR altitude IS NULL );
 ALTER TABLE qwat_od.cover ADD CONSTRAINT chk_cover_altitude_precisionalti CHECK (fk_precisionalti IS NOT NULL OR altitude IS NULL );
