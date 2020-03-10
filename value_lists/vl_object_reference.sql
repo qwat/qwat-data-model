9678c7561b8f3bf2d738653eb366ed1ec0471605
Bertrand Rix <bertrand.rix@oslandia.com>
2020-03-10 16:37:09 +0100

    Add 4 new elements to qwat_vl.object_reference

--- value_lists/vl_object_reference.sql
+++ value_lists/vl_object_reference.sql
@@ -18,7 +18,8 @@ INSERT INTO qwat_vl.object_reference (id, value_fr, value_en, value_ro) VALUES (
 INSERT INTO qwat_vl.object_reference (id, value_fr, value_en, value_ro) VALUES (9003,'dessus de la conduite','on top of pipe','pe conductă');
 INSERT INTO qwat_vl.object_reference (id, value_fr, value_en, value_ro) VALUES (9004,'dessus','on top','deasupra');
 INSERT INTO qwat_vl.object_reference (id, value_fr, value_en, value_ro) VALUES (9005,'axe de la conduite','pipe axis','axul conductei');
-
-
-
+INSERT INTO qwat_vl.object_reference (id, value_fr, value_en, value_ro) VALUES (9010,'fond de fouille','excavation bed','bază săpătură');
+INSERT INTO qwat_vl.object_reference (id, value_fr, value_en, value_ro) VALUES (9020,'sur vanne','on valve','pe supapă');
+INSERT INTO qwat_vl.object_reference (id, value_fr, value_en, value_ro) VALUES (9021,'sur tige de vanne','on valve shaft','pe arborele supapei');
+INSERT INTO qwat_vl.object_reference (id, value_fr, value_en, value_ro) VALUES (9022,'sur cape de vanne','on valve bonnet','pe capota supapelor');
 
