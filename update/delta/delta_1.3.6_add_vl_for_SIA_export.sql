f05fc387465065e3f13009b0923c28789a7b4370
olivierdalang <olivier.dalang@gmail.com>
2021-01-06 15:34:19 +0100

    [SIAEXPORT] stub for value lists

--- update/delta/delta_1.3.6_add_vl_for_SIA_export.sql
+++ update/delta/delta_1.3.6_add_vl_for_SIA_export.sql
@@ -1,4 +1,4 @@
 ALTER TABLE qwat_vl.pipe_material ADD COLUMN sia_de text not null default 'unbekannt'; 
 ALTER TABLE qwat_vl.pipe_material ADD COLUMN sia_fr text not null default 'inconnu';
 
-UPDATE qwat_vl.pipe_material SET sia_de = "Polyethylen", sia_fr = "polyethylene" WHERE short_fr = 'PE';
+UPDATE qwat_vl.pipe_material SET sia_de = 'Polyethylen', sia_fr = 'polyethylene' WHERE short_fr = 'PE';
