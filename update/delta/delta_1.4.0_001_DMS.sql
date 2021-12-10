d89a99370326e632e77f6038393830ef651fec9f
Denis Rouzaud <denis.rouzaud@gmail.com>
2021-12-10 14:21:11 +0100

    add delta for DMS

---
+++ update/delta/delta_1.4.0_001_DMS.sql
@@ -0,0 +1,20 @@
+CREATE TABLE qwat_od.document(
+  id uuid DEFAULT gen_random_uuid() NOT NULL,
+  identification text NOT NULL,
+  path text,
+  type text
+);
+ALTER TABLE ONLY qwat_od.document ADD CONSTRAINT document_pkey PRIMARY KEY (id);
+
+
+CREATE TABLE qwat_od.rel_document(
+  id uuid DEFAULT gen_random_uuid() NOT NULL,
+  fk_document uuid,
+  object_class text,
+  fk_object text
+);
+ALTER TABLE ONLY qwat_od.rel_document ADD CONSTRAINT rel_document_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY qwat_od.rel_document
+  ADD CONSTRAINT fkey_od_document FOREIGN KEY (fk_document)
+  REFERENCES qwat_od.document(id) MATCH FULL DEFERRABLE INITIALLY DEFERRED;
