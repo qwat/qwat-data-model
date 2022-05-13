134c5bdfbf12265f22684c9f2e48840fc7024e4f
kevin.andre.gx@gmail.com <kevin.andre.gx@gmail.com>
2022-05-13 11:54:19 +0200

    add delta file for fix diameter values

---
+++ update/delta/delta_1.4.0_004_fix_nominal_diameter_values_typo.sql
@@ -0,0 +1,5 @@
+UPDATE qwat_vl.nominal_diameter
+SET value_fr = 'NPS: 8 DN: 200 OD: 219.08',
+    value_en = 'NPS: 8 DN: 200 OD: 219.08',
+    value_ro = 'NPS: 8 DN: 200 OD: 219.08'
+WHERE id = 7013;
