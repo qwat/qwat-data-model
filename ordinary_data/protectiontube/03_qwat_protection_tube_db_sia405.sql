c34ffaed970e2959ff92d1f53553407f9c63b468
SJiB <stefan.burckhardt@gmx.ch>
2024-11-16 19:08:05 +0100

    add reference from protection_tube to pipe

--- ordinary_data/protectiontube/03_qwat_protection_tube_db_sia405.sql
+++ ordinary_data/protectiontube/03_qwat_protection_tube_db_sia405.sql
@@ -104,6 +104,10 @@ ALTER TABLE qwat_vl.sia405pt_protection_tube_status ADD CONSTRAINT pkey_qwat_vl_
 ALTER TABLE qwat_od.sia405pt_protection_tube ADD CONSTRAINT rel_od_sia405pt_protection_tube_fk_dataowner FOREIGN KEY (fk_dataowner) REFERENCES qwat_od.organisation(obj_id) DEFERRABLE INITIALLY DEFERRED;
 ALTER TABLE qwat_od.sia405pt_protection_tube ADD CONSTRAINT rel_od_sia405pt_protection_tube_fk_dataprovider FOREIGN KEY (fk_provider) REFERENCES qwat_od.organisation(obj_id) DEFERRABLE INITIALLY DEFERRED;
 
+--------- Extra Relation to class pipe of tdh
+ALTER TABLE qwat_od.sia405pt_protection_tube ADD COLUMN fk_pipe varchar(16);
+ALTER TABLE qwat_od.sia405pt_protection_tube ADD CONSTRAINT rel_od_sia405pt_protection_tube_fk_pipe FOREIGN KEY (fk_pipe) REFERENCES qwat_od.pipe(id) DEFERRABLE INITIALLY DEFERRED;
+
 ------ Indexes on identifiers
 
  CREATE UNIQUE INDEX in_od_sia405pt_protection_tube_name_number ON qwat_od.sia405pt_protection_tube USING btree (name_number ASC NULLS LAST, fk_dataowner ASC NULLS LAST);
